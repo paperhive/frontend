115df33293ade8aa8a8a84cfdaf88a3e32fb4287
Nico Schlömer <nico.schloemer@gmail.com>
2014-10-30 16:57:30 +0100

    don't define function in loop

--- js/viewer.js
+++ js/viewer.js
@@ -13,40 +13,41 @@
         function(pdf) {
           var canvasWrapper = document.getElementById('the-canvas-wrapper');
           var wrapperWidth = canvasWrapper.offsetWidth;
+          var showPage = function(page) {
+            // Scale such that the width of the viewport is the fills the
+            // wrapper.
+            var scale = 1.0;
+            var viewport = page.getViewport(scale);
+            scale = wrapperWidth / viewport.width;
+            viewport = page.getViewport(scale);
+
+            // Prepare canvas using PDF page dimensions
+            //var canvas = document.getElementById('the-canvas');
+            var canvas = document.createElement( "canvas" );
+
+            //canvas.style.display = "block";
+            canvas.style.cssText = "border:1px solid black;";
+            var context = canvas.getContext('2d');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            // Render PDF page into canvas context
+            var renderContext = {
+              canvasContext: context,
+              viewport: viewport
+            };
+            page.render(renderContext);
+            // Append to page
+            //document.body.appendChild( canvas );
+            canvasWrapper.appendChild(canvas);
+          };
+
           for(var i = 0; i < pdf.numPages; i++){
             // Using promise to fetch the page
-            pdf.getPage(i).then(function(page) {
-              // Scale such that the width of the viewport is the fills the
-              // wrapper.
-              var scale = 1.0;
-              var viewport = page.getViewport(scale);
-              scale = wrapperWidth / viewport.width;
-              console.log(scale);
-              viewport = page.getViewport(scale);
-
-              // Prepare canvas using PDF page dimensions
-              //var canvas = document.getElementById('the-canvas');
-              var canvas = document.createElement( "canvas" );
-
-              //canvas.style.display = "block";
-              canvas.style.cssText = "border:1px solid black;";
-              var context = canvas.getContext('2d');
-              canvas.height = viewport.height;
-              canvas.width = viewport.width;
-
-              // Render PDF page into canvas context
-              var renderContext = {
-                canvasContext: context,
-                viewport: viewport
-              };
-              page.render(renderContext);
-              // Append to page
-              //document.body.appendChild( canvas );
-              canvasWrapper.appendChild(canvas);
-            });
+            pdf.getPage(i).then(showPage);
           }
         });
-      };
-    });
-  }
+    };
+  });
+}
 )();
