6c29d2c979e1e797b43facad4a68bec08c257ee8
André Gaul <andre@gaul.io>
2014-11-18 18:07:20 +0100

    add default route

--- src/js/config/routes.js
+++ src/js/config/routes.js
@@ -1,44 +1,48 @@
 module.exports = function (app) {
 
-  app.config(
-    ['$routeSegmentProvider', function($routeSegmentProvider) {
-    $routeSegmentProvider
-    .when('/oauth/orcid', 'oauth')
-    .when('/articles', 'article')
-    .when('/articles/:id', 'article.info')
-    .when('/articles/:id/text', 'article.text')
-    .when('/articles/:id/annotations', 'article.annotations')
-    .when('/articles/:id/annotations/new', 'article.annotation-new')
-    .when('/articles/:id/settings', 'article.settings')
+  app.config([
+    '$routeSegmentProvider', '$routeProvider',
+    function($routeSegmentProvider, $routeProvider) {
+      $routeSegmentProvider
+        .when('/oauth/orcid', 'oauth')
+        .when('/articles', 'article')
+        .when('/articles/:id', 'article.info')
+        .when('/articles/:id/text', 'article.text')
+        .when('/articles/:id/annotations', 'article.annotations')
+        .when('/articles/:id/annotations/new', 'article.annotation-new')
+        .when('/articles/:id/settings', 'article.settings')
 
-    .segment('oauth', {
-      templateUrl: 'templates/oauth.html',
-      controller: 'OauthOrcidCtrl'
-    })
+        .segment('oauth', {
+          templateUrl: 'templates/oauth.html',
+          controller: 'OauthOrcidCtrl'
+        })
 
-    .segment('article', {
-      templateUrl: 'templates/article.html'
-    })
-    .within()
-    .segment('text', {
-      default: true,
-      templateUrl: 'templates/text.html',
-      controller: 'DisplayCtrl',
-      dependencies: ['id']
-    })
-    .segment('annotations', {
-      templateUrl: 'templates/annotation-list.html',
-      controller: 'IssueListCtrl',
-      dependencies: ['id']
-    })
-    .segment('annotation-new', {
-      templateUrl: 'templates/annotation-new.html',
-      dependencies: ['id']
-    })
-    .segment('settings', {
-      templateUrl: 'templates/settings.html',
-      dependencies: ['id']
-    })
-    ;
-  }]);
+        .segment('article', {
+          templateUrl: 'templates/article.html'
+        })
+        .within()
+        .segment('text', {
+          default: true,
+          templateUrl: 'templates/text.html',
+          controller: 'DisplayCtrl',
+          dependencies: ['id']
+        })
+        .segment('annotations', {
+          templateUrl: 'templates/annotation-list.html',
+          controller: 'IssueListCtrl',
+          dependencies: ['id']
+        })
+        .segment('annotation-new', {
+          templateUrl: 'templates/annotation-new.html',
+          dependencies: ['id']
+        })
+        .segment('settings', {
+          templateUrl: 'templates/settings.html',
+          dependencies: ['id']
+        })
+        ;
+
+      $routeProvider.otherwise({redirectTo: '/articles/0af5e13'});
+    }
+  ]);
 };
