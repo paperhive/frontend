479844e1f7f31ca47fcd935816a7535ab2bcd4cb
André Gaul <andre@gaul.io>
2014-11-19 16:43:27 +0100

    auth: save token to session/local storage

--- src/js/services/auth.js
+++ src/js/services/auth.js
@@ -1,12 +1,12 @@
 module.exports = function (app) {
-  app.factory('AuthService', ['config', '$http', '$q',
-    function (config, $http, $q) {
+  app.factory('AuthService', ['config', '$http', '$q', '$window',
+    function (config, $http, $q, $window) {
       var authService = {
         inProgress: false,
         orcidUrl: config.api_url + '/oauth/orcid'
       };
 
-      authService.signinOrcid = function (code, state) {
+      function signin(url, data, config) {
         var deferred = $q.defer();
         if (authService.inProgress) {
           return deferred.reject('Already signing in.');
@@ -16,22 +16,60 @@ module.exports = function (app) {
         }
         authService.inProgress = true;
         $http
-          .post(
-            config.api_url + '/oauth/orcid/signin',
-            {code: code, state: state},
-            {timeout: 10000}
-          )
+          .post(url, data, config)
           .success(function (data, status) {
             authService.inProgress = false;
-            authService.user = data;
+            authService.user = data.user;
+
+            // save token
+            $window.sessionStorage.token = data.token;
+            if (data.user.settings.remember) {
+              $window.localStorage.token = data.token;
+            }
+
+            // use token for all subsequent HTTP requests to API
+            $http.defaults.headers.common['X-Auth-Token'] = data.token;
+
             deferred.resolve(data);
           })
           .error(function (data) {
             authService.inProgress = false;
+            signout();
             deferred.reject('signing in failed');
           });
 
         return deferred.promise;
+      }
+
+      function signout() {
+        delete $window.sessionStorage.token;
+        delete $window.localStorage.token;
+        delete $http.defaults.headers['X-Auth-Token'];
+        delete authService.user;
+      }
+
+      // grab token from session or local storage
+      var token = $window.sessionStorage.token || $window.localStorage.token;
+
+      // sign in if token is present
+      if (token) {
+        signin(
+          config.api_url + '/signin',
+          null,
+          {
+            headers: {'X-Auth-Token': token},
+            timeout: 10000
+          }
+        );
+      }
+
+      // sign in function for ORCID viaOAuth
+      authService.signinOrcid = function (code, state) {
+        return signin(
+          config.api_url + '/oauth/orcid/signin',
+          {code: code, state: state},
+          {timeout: 10000}
+        );
       };
 
       return authService;
