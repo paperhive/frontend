c859c2190902a8173aa98435789800c893ae0658
Nico Schlömer <nico.schloemer@gmail.com>
2014-11-20 13:29:33 +0100

    annotation list header

--- src/js/controllers/annotation.js
+++ src/js/controllers/annotation.js
@@ -1,4 +1,14 @@
 module.exports = function (app) {
   app.controller('AnnotationCtrl', [function() {
+    this.annotations = [];
+
+    this.addAnnotation = function(annotation) {
+      this.annotati
+      return;
+    };
+
+    this.getAnnotations = function() {
+      return this.annotations;
+    };
   }]);
 };
--- src/js/controllers/controlbar.js
+++ src/js/controllers/controlbar.js
@@ -1,7 +1,6 @@
 module.exports = function (app) {
   app.controller('ControlbarCtrl', [function() {
     this.tab = 1;
-    console.log("dasdas");
 
     this.isSet = function(checkTab) {
       return this.tab === checkTab;
