7fe2fa06eba66ee20dc66ef3f795ea9de40823e7
Nico Schlömer <nico.schloemer@gmail.com>
2015-01-27 20:01:05 +0100

    [tests] land in settings to avoid PDF loading

--- test/spec.js
+++ test/spec.js
@@ -1,6 +1,6 @@
 describe('PaperHub homepage', function() {
   it('Test page title', function() {
-    browser.get('/#/articles/0af5e13/comments');
+    browser.get('/#/articles/0af5e13/settings');
 
     expect(browser.getTitle()).toEqual('PaperHub');
   });
