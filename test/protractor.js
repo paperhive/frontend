1616bef119b005e2bbaed5b297bc64b115adcf19
Nico Schlömer <nico.schloemer@gmail.com>
2015-01-29 10:37:29 +0100

    Revert "[tests] don't set seleniumAddress in export.config for saucelabs"
    This reverts commit 4dfc93ba84342c55f4822a4bd9e5315b29d602f9.

--- test/protractor.js
+++ test/protractor.js
@@ -1,10 +1,13 @@
-// Create tests list
 if (process.env.TRAVIS_JOB_NUMBER) {
   // Travis tests, forwarded to SauceLabs. Test multiple browsers.
   var browsers = ['chrome', 'iexplore', 'safari'];
+  // Can't specify seleniumAddress for saucelabs, see
+  // <http://stackoverflow.com/a/20889537/353337>.
+  var seleniumAddress = null;
 } else {
   // only tests chrome locally
   var browsers = ['chrome'];
+  var seleniumAddress = 'http://localhost:4444/wd/hub';
 }
 var tests = [];
 browsers.forEach(
@@ -30,11 +33,6 @@ exports.config = {
     defaultTimeoutInterval: 60000
   },
 
+  seleniumAddress: seleniumAddress,
   baseUrl: 'http://localhost:' + (process.env.HTTP_PORT || '8080')
 };
-
-if (!process.env.TRAVIS_JOB_NUMBER) {
-  // Only specify seleniumAddress locally, not for saucelabs,
-  // cf. <http://stackoverflow.com/a/20889537/353337>.
-  exports.config.seleniumAddress = 'http://localhost:4444/wd/hub';
-}
