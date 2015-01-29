3980bbed7bb85a98cc64c611d24d8bb4dae81bbc
Nico Schlömer <nico.schloemer@gmail.com>
2015-01-29 10:37:58 +0100

    Revert "[tests] only test chrome locally"
    This reverts commit 04b7d8293b8923bd67c1487b2372b5ec1c0ada25.

--- test/protractor.js
+++ test/protractor.js
@@ -1,30 +1,59 @@
-if (process.env.TRAVIS_JOB_NUMBER) {
-  // Travis tests, forwarded to SauceLabs. Test multiple browsers.
-  var browsers = ['chrome', 'iexplore', 'safari'];
-  // Can't specify seleniumAddress for saucelabs, see
-  // <http://stackoverflow.com/a/20889537/353337>.
-  var seleniumAddress = null;
-} else {
-  // only tests chrome locally
-  var browsers = ['chrome'];
-  var seleniumAddress = 'http://localhost:4444/wd/hub';
-}
-var tests = [];
-browsers.forEach(
-  function(entry) {
-  tests.pop({
-    'browserName': entry,
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    'build': process.env.TRAVIS_BUILD_NUMBER,
-    'name': 'PaperHub (' + entry + ')'
-  });}
-);
-
 exports.config = {
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
 
-  multiCapabilities: tests,
+  multiCapabilities: [
+  //  {
+  //  'browserName': 'android',
+  //  'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+  //  'build': process.env.TRAVIS_BUILD_NUMBER,
+  //  'name': 'PaperHub (Android)'
+  //},
+  {
+    'browserName': 'chrome',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'PaperHub (Chrome)'
+  },
+  {
+    'browserName': 'firefox',
+    // http://stackoverflow.com/a/27645817/353337
+    'version': "34",
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'PaperHub (Firefox)'
+  },
+  {
+    'browserName': 'iexplore',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'PaperHub (Internet Explorer)'
+  },
+  //{
+  //  'browserName': 'ipad',
+  //  'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+  //  'build': process.env.TRAVIS_BUILD_NUMBER,
+  //  'name': 'PaperHub (iPad)'
+  //},
+  //{
+  //  'browserName': 'iphone',
+  //  'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+  //  'build': process.env.TRAVIS_BUILD_NUMBER,
+  //  'name': 'PaperHub (iPhone)'
+  //},
+  //{
+  //  'browserName': 'opera',
+  //  'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+  //  'build': process.env.TRAVIS_BUILD_NUMBER,
+  //  'name': 'PaperHub (Opera)'
+  //},
+  {
+    'browserName': 'safari',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+    'name': 'PaperHub (Safari)'
+  },
+  ],
 
   specs: ['spec.js'],
 
@@ -33,6 +62,8 @@ exports.config = {
     defaultTimeoutInterval: 60000
   },
 
-  seleniumAddress: seleniumAddress,
+  // Can't specify seleniumAddress, see
+  // <http://stackoverflow.com/a/20889537/353337>.
+  //seleniumAddress: 'http://localhost:4444/wd/hub',
   baseUrl: 'http://localhost:' + (process.env.HTTP_PORT || '8080')
 };
