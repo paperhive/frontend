2e4edac2e0b8a8dd574eb614780b131dee5ac97a
André Gaul <andre@gaul.io>
2015-01-31 13:41:25 +0100

    [navbar] fix collapsed navbar

--- src/js/controllers/index.js
+++ src/js/controllers/index.js
@@ -3,6 +3,7 @@ module.exports = function (app) {
   require('./article.js')(app);
   require('./discussion.js')(app);
   require('./marginnote.js')(app);
+  require('./navbar.js')(app);
   require('./oauthOrcid.js')(app);
   require('./settings.js')(app);
   require('./user.js')(app);
