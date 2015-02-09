f53c4ed86fa998df202a2a1e69fa0fb7a647a8f1
Daniel Pfisterer <info@daniel-pfisterer.de>
2015-02-09 20:03:24 +0100

    [Package] Added angular-strap-(affix | scrollspy)

--- src/js/index.js
+++ src/js/index.js
@@ -12,6 +12,10 @@
   require('angular-animate'); // provides 'ngAnimate' module
   require('angular-bootstrap-tpls'); // provides 'ui.bootstrap' module
   require('angular-sanitize'); // provides 'ngSanitize' module
+  require('angular-strap-affix'); // provides 'mgcrea.ngStrap.affix' module
+  require('angular-strap-debounce'); // provides 'mgcrea.ngStrap.helpers.debounce' module
+  require('angular-strap-dimensions'); // provides 'mgcrea.ngStrap.helpers.dimensions' module
+  require('angular-strap-scrollspy'); // provides 'mgcrea.ngStrap.scrollspy' module
   require('angular-route'); // provides 'ngRoute' module
   require('angular-moment');
   require('detect-element-resize'); // injects resize+removeResize to jquery
@@ -33,7 +37,9 @@
        'route-segment',
        'view-segment',
        'angularMoment',
-       'templates'
+       'templates',
+       'mgcrea.ngStrap.affix',
+       'mgcrea.ngStrap.scrollspy'
       ]
     )
     .constant('config', require('../../config.json'))
