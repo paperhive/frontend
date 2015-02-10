67b79f477c0ac00c8211c17df236d71425cc8694
Daniel Pfisterer <info@daniel-pfisterer.de>
2015-02-10 12:07:33 +0100

    [JS] changed anchorlink offset to top

--- src/js/controllers/sidenav.js
+++ src/js/controllers/sidenav.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.controller('SideNavCtrl', ['$anchorScroll', '$location', '$scope',
     function ($anchorScroll, $location, $scope) {
       $scope.gotoAnchor = function(anchorLinkName) {
-        $anchorScroll.yOffset = 100;   // always scroll by 50 extra pixels
+        $anchorScroll.yOffset = 98;
         var newHash = anchorLinkName;
         $location.hash(anchorLinkName);
         $anchorScroll();
