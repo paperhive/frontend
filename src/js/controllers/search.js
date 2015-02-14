5df352d7cd47fc76ddb4540b49e7a16af68608ee
Daniel Pfisterer <info@daniel-pfisterer.de>
2015-02-14 15:28:06 +0100

    [JS] Added Search Ctrl, datepicker and collapser

---
+++ src/js/controllers/search.js
@@ -0,0 +1,43 @@
+module.exports = function (app) {
+  app.controller('AdvancedSearchCtrl', ['$scope', function ($scope) {
+    $scope.isCollapsed = true;
+  }]);
+  app.controller('DatepickerCtrl', ['$scope', function ($scope) {
+
+    $scope.today = function() {
+      $scope.dt = new Date();
+    };
+    $scope.today();
+
+    $scope.clear = function () {
+      $scope.dt = null;
+    };
+
+    // Disable weekend selection
+    $scope.disabled = function(date, mode) {
+      return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+    };
+
+    $scope.toggleMin = function() {
+      $scope.minDate = $scope.minDate ? null : new Date();
+    };
+    $scope.toggleMin();
+
+    $scope.open = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+
+      $scope.opened = true;
+    };
+
+    $scope.dateOptions = {
+      formatYear: 'yy',
+      startingDay: 1
+    };
+
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
+
+  }]);
+
+};
