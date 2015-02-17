579ff15b51409aa495537c5f6710ab1faba6c213
André Gaul <andre@gaul.io>
2015-02-17 18:13:12 +0100

    [controller] remove debug code
    yey, this was the fun part! \o/

--- src/js/controllers/user.js
+++ src/js/controllers/user.js
@@ -1,40 +1,4 @@
 module.exports = function (app) {
-  // BEGIN debug code
-  allUsers = [
-    {
-      _id: "08ea4",
-      orcidId: "dasdasd0",
-      userName: "hondi",
-      displayName: "Hondanz",
-      gravatarMd5: ""
-    },
-    {
-      _id: "152ea4",
-      orcidId: "dasdasd1",
-      userName:  "hoppenstedt",
-      displayName: "Opa Hoppenstedt",
-      gravatarMd5: ""
-    },
-    {
-      _id: "ea5411",
-      orcidId: "dasdasd2",
-      userName: "jimmy",
-      displayName: "Jimmy",
-      email: "jimmy@best.com",
-      gravatarMd5: ""
-    }
-  ];
-  // create lookup array for debugging purposes
-  var userLookup = {};
-  for (var i = 0, len = allUsers.length; i < len; i++) {
-    userLookup[allUsers[i].userName] = allUsers[i];
-  }
-  var userLookupById = {};
-  for (var i = 0, len = allUsers.length; i < len; i++) {
-    userLookupById[allUsers[i]._id] = allUsers[i];
-  }
-  // END debug code
-
   app.controller('UserCtrl', [
     '$scope', '$routeSegment', 'config', '$http', 'NotificationsService',
     function ($scope, $routeSegment, config, $http, notificationsService) {
