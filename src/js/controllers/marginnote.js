8e168a0627703333f152d5985b9b512f00059922
Nico Schlömer <nico.schloemer@gmail.com>
2015-02-18 18:25:25 +0100

    unHighlightSelection -> phUnhighlightSelection

--- src/js/controllers/marginnote.js
+++ src/js/controllers/marginnote.js
@@ -18,7 +18,7 @@ module.exports = function (app) {
         highlighter.highlightSelection("ph-highlight", $scope.userSelection);
       };
 
-      $scope.unHighlightSelection = function() {
+      $scope.phUnhighlightSelection = function() {
         highlighter.unhighlightSelection(userSelection);
       };
     }]);
