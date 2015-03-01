6fdf1be2d8167d726cffc552b573b31fef2260b0
André Gaul <andre@gaul.io>
2015-03-01 18:06:57 +0100

    [js,html] add approved article sources import

--- src/js/controllers/articleNew.js
+++ src/js/controllers/articleNew.js
@@ -1,17 +1,58 @@
 module.exports = function (app) {
   app.controller('ArticleNewCtrl', [
-    '$scope', '$http', 'config', 'authService',
-    function($scope, $http, config, authService) {
+    '$scope', '$http', '$q', '$location', 'config', 'authService',
+    'NotificationsService',
+    function($scope, $http, $q, $location, config, authService,
+             NotificationService) {
+      // used to cancel running http requests
+      var canceler;
+
       $scope.$watch('handle', function (handle) {
-        $http.get(config.api_url + '/articles/external/' +
-                  encodeURIComponent(handle))
-          .success(function (metadata) {
-            $scope.metadata = metadata;
-          })
-          .error(function () {
-            $scope.metadata = undefined;
-          });
+        $scope.article = undefined;
+
+        // collapse metadata panel (nothing to view!)
+        $scope.metadataCollapsed = true;
+
+        if (handle) {
+          // cancel running http request
+          if (canceler) {
+            canceler.resolve();
+          }
+
+          // initiate http request
+          canceler = $q.defer();
+          $http.get(
+            config.api_url + '/articles/external/' + encodeURIComponent(handle),
+            {timeout: canceler.promise}
+          )
+            .success(function (article) {
+              $scope.article = article;
+            })
+            .error(function () {
+              $scope.article = undefined;
+            });
+        }
       });
+
+      $scope.submitApproved = function () {
+        $scope.submitting = true;
+        $http.post(
+          config.api_url + '/articles/external/' +
+          encodeURIComponent($scope.handle)
+        )
+          .success(function (article) {
+            $scope.submitting = false;
+            $location.path('/articles/' + article._id);
+          })
+          .error(function (data) {
+            $scope.submitting = false;
+            NotificationsService.notifications.push({
+              type: 'error',
+              message: data.message || 'could not add article (unknown reason)'
+            });
+          });
+
+      };
     }
   ]);
 };
