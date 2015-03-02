6e0dfa3c4f00f789bf0f9737172283e73d194db1
Nico Schlömer <nico.schloemer@gmail.com>
2015-03-02 22:38:53 +0100

    remove quotes around user display name in gravatar alt

--- src/js/directives/gravatar.js
+++ src/js/directives/gravatar.js
@@ -25,7 +25,7 @@ module.exports = function (app) {
               "?s=" + scope.gravatarSize +
               "&d=identicon"
             );
-            element.attr('alt', "\"" + user.displayName + " avatar\"");
+            element.attr('alt', user.displayName + " avatar");
           }
         );
       }
