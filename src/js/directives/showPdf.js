52ebea48a44ee13e9fbcc05ea41bf6cbda6cc8ea
André Gaul <andre@gaul.io>
2015-03-03 22:16:57 +0100

    [js,html] minor modifications in pdf directive
    use attrs instead of isolated scope

--- src/js/directives/showPdf.js
+++ src/js/directives/showPdf.js
@@ -1,184 +1,169 @@
 module.exports = function (app) {
-  app.directive('pdf', [
-    '$rootScope',
-    function ($rootScope) {
+  app.directive('pdf', function () {
     return {
       restrict: 'E',
-      scope: {
-        'url': '@',
-        'onMouseup': '&',
-        'simple': '@'
-      },
       link: function (scope, element, attrs) {
-        if (scope.url === undefined) {
-          return;
-        }
+        scope.$watch('url', renderPdf);
+        scope.$watch('textOverlay', renderPdf);
 
-        // From
-        // <https://github.com/mozilla/pdf.js/blob/master/examples/components/simpleviewer.js>.
-        if (!PDFJS.PDFViewer || !PDFJS.getDocument) {
-          alert('Please build the library and components using\n' +
-                '  `node make generic components`');
-        }
+        function renderPdf () {
+          var url = scope.$eval(attrs.url);
+          var textOverlay = scope.$eval(attrs.textOverlay);
+          if (!url) return;
 
-        if (scope.simple) {
-          // Simple viewer without any overlays.
+          // From
+          // <https://github.com/mozilla/pdf.js/blob/master/examples/components/simpleviewer.js>.
 
-          // Fetch the PDF document from the URL using promises
-          PDFJS.getDocument(scope.url).then(function(pdf) {
-            var wrapperWidth = element[0].offsetWidth;
-            if (wrapperWidth === 0) {
-              // TODO make sure this error doesn't get silently intercepted
-              console.log("Invalid wrapper width");
-              throw Error("Invalid wrapper width");
-            }
+          if (!textOverlay) {
+            // Simple viewer without any overlays.
 
-            var showPage = function(page) {
-              // Scale such that the width of the viewport is the fills the
-              // wrapper.
-              var scale = 1.0;
-              var viewport = page.getViewport(scale);
-              scale = wrapperWidth / viewport.width;
-              viewport = page.getViewport(scale);
+            // Fetch the PDF document from the URL using promises
+            PDFJS.getDocument(url).then(function (pdf) {
+              var wrapperWidth = element[0].offsetWidth;
+              if (wrapperWidth === 0) {
+                // TODO make sure this error doesn't get silently intercepted
+                console.log("Invalid wrapper width");
+                throw Error("Invalid wrapper width");
+              }
 
-              // Prepare canvas using PDF page dimensions
-              var link = document.createElement("a");
-              // From <http://stackoverflow.com/a/14717011/353337>
-              // link.setAttribute("href", "#/articles/0af5e13/text?scrollTo=pageContainer".concat(page.pageIndex+1));
-              link.setAttribute("href", "#/articles/0af5e13/text");
-              var canvas = document.createElement("canvas");
-              canvas.setAttribute("class", "ph-page");
-              link.appendChild(canvas);
+              var showPage = function(page) {
+                // Scale such that the width of the viewport is the fills the
+                // wrapper.
+                var scale = 1.0;
+                var viewport = page.getViewport(scale);
+                scale = wrapperWidth / viewport.width;
+                viewport = page.getViewport(scale);
 
-              var context = canvas.getContext('2d');
-              canvas.height = viewport.height;
-              canvas.width = viewport.width;
+                // Prepare canvas using PDF page dimensions
+                var link = document.createElement("a");
+                // From <http://stackoverflow.com/a/14717011/353337>
+                // link.setAttribute("href", "#/articles/0af5e13/text?scrollTo=pageContainer".concat(page.pageIndex+1));
+                link.setAttribute("href", "#/articles/0af5e13/text");
+                var canvas = document.createElement("canvas");
+                canvas.setAttribute("class", "ph-page");
+                link.appendChild(canvas);
 
-              // Render PDF page into canvas context
-              var renderContext = {
-                canvasContext: context,
-                viewport: viewport
+                var context = canvas.getContext('2d');
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;
+
+                // Render PDF page into canvas context
+                var renderContext = {
+                  canvasContext: context,
+                  viewport: viewport
+                };
+                page.render(renderContext);
+                // Append to page
+                element.append(link);
               };
-              page.render(renderContext);
-              // Append to page
-              element.append(link);
-            };
 
-            for(var i = 1; i < pdf.numPages; i++){
-              // Using promise to fetch the page
-              pdf.getPage(i).then(showPage);
-            }
-          });
-        } else {
-          // Complex viewer with bells & whistles
-          var container = document.createElement("div");
-          container.id = "pdfContainer";
-          var viewer = document.createElement("div");
-          viewer.id = "viewer";
-          viewer.className = "pdfViewer";
-          container.appendChild(viewer);
+              for(var i = 1; i < pdf.numPages; i++){
+                // Using promise to fetch the page
+                pdf.getPage(i).then(showPage);
+              }
+            });
+          } else {
+            // Complex viewer with bells & whistles
+            var container = document.createElement("div");
+            container.id = "pdfContainer";
+            var viewer = document.createElement("div");
+            viewer.id = "viewer";
+            viewer.className = "pdfViewer";
+            container.appendChild(viewer);
 
-          element.append(container);
+            element.append(container);
 
-          var pdfViewer = new PDFJS.PDFViewer({
-            container: container
-          });
+            var pdfViewer = new PDFJS.PDFViewer({
+              container: container
+            });
 
-          container.addEventListener('pagesinit', function () {
-            pdfViewer.currentScaleValue = 'page-width';
-          });
+            container.addEventListener('pagesinit', function () {
+              pdfViewer.currentScaleValue = 'page-width';
+            });
 
-          // Loading document.
-          var pdf = PDFJS.getDocument(scope.url);
-          pdf.then(function (pdfDocument) {
-            // Document loaded, specifying document for the viewer.
-            pdfViewer.setDocument(pdfDocument);
-          });
+            // Loading document.
+            PDFJS.getDocument(url).then(function (pdf) {
+              // Document loaded, specifying document for the viewer.
+              pdfViewer.setDocument(pdf);
+            });
 
-          // --------------------------------------------------------------------
-          //// Get text width
-          //pdf.then(function(pdf) {
-          //  var maxPages = pdf.pdfInfo.numPages;
-          //  maxPages = 1;
-          //  for (var j = 1; j <= maxPages; j++) {
-          //    var page = pdf.getPage(j);
-          //    page.then(function(page) {
-          //      var textContent = page.getTextContent();
-          //      var viewport = page.getViewport(pdfViewer.currentScale);
-          //      textContent.then(function(content) {
-          //        for (var i = 0; i < content.items.length; i++) {
-          //          // Well...
+            // --------------------------------------------------------------------
+            //// Get text width
+            //pdf.then(function(pdf) {
+            //  var maxPages = pdf.pdfInfo.numPages;
+            //  maxPages = 1;
+            //  for (var j = 1; j <= maxPages; j++) {
+            //    var page = pdf.getPage(j);
+            //    page.then(function(page) {
+            //      var textContent = page.getTextContent();
+            //      var viewport = page.getViewport(pdfViewer.currentScale);
+            //      textContent.then(function(content) {
+            //        for (var i = 0; i < content.items.length; i++) {
+            //          // Well...
 
-          //          // Check out the discussion at
-          //          // https://github.com/mozilla/pdf.js/issues/5643#issuecomment-69969258
-          //          // for more details.
-          //          console.log('');
-          //          // Translate PDF transform into the screen presentation
-          //          // transform
-          //          //console.log(viewport.transform);
-          //          //console.log(content.items[i].transform);
-          //          tx = PDFJS.Util.transform(
-          //            viewport.transform,
-          //            content.items[i].transform
-          //          );
-          //          var fontHeight = Math.sqrt((tx[2] * tx[2]) + (tx[3] * tx[3]));
-          //          //console.log(tx[2]);
-          //          //console.log(tx[3]);
-          //          //console.log(fontHeight);
-          //          // The transformation matrix is specified as an array of
-          //          // length 6 just like CSS3 transforms, cf.
-          //          // <https://dev.opera.com/articles/understanding-the-css-transforms-matrix/>.
-          //          console.log(content.items[i]);
-          //          // x-position of the top-right point
-          //          var extent = tx[4];
-          //          //  tx[0] * content.items[i].width/fontHeight +
-          //          //  tx[4];
-          //          console.log("tx[4] = ", extent);
-          //          var actualWidth = pdfViewer.currentScale * content.items[i].width;
-          //        }
-          //      })
-          //    })
-          //  }
-          //});
-          // --------------------------------------------------------------------
+            //          // Check out the discussion at
+            //          // https://github.com/mozilla/pdf.js/issues/5643#issuecomment-69969258
+            //          // for more details.
+            //          console.log('');
+            //          // Translate PDF transform into the screen presentation
+            //          // transform
+            //          //console.log(viewport.transform);
+            //          //console.log(content.items[i].transform);
+            //          tx = PDFJS.Util.transform(
+            //            viewport.transform,
+            //            content.items[i].transform
+            //          );
+            //          var fontHeight = Math.sqrt((tx[2] * tx[2]) + (tx[3] * tx[3]));
+            //          //console.log(tx[2]);
+            //          //console.log(tx[3]);
+            //          //console.log(fontHeight);
+            //          // The transformation matrix is specified as an array of
+            //          // length 6 just like CSS3 transforms, cf.
+            //          // <https://dev.opera.com/articles/understanding-the-css-transforms-matrix/>.
+            //          console.log(content.items[i]);
+            //          // x-position of the top-right point
+            //          var extent = tx[4];
+            //          //  tx[0] * content.items[i].width/fontHeight +
+            //          //  tx[4];
+            //          console.log("tx[4] = ", extent);
+            //          var actualWidth = pdfViewer.currentScale * content.items[i].width;
+            //        }
+            //      })
+            //    })
+            //  }
+            //});
+            // --------------------------------------------------------------------
 
-          // --------
-          //var container = document.createElement("div");
-          //container.id = "container";
-          //element.append(container);
+            // --------
+            //var container = document.createElement("div");
+            //container.id = "container";
+            //element.append(container);
 
-          //// Loading document.
-          //var PAGE_TO_VIEW = 1;
-          //var SCALE = 1.0;
-          //PDFJS.getDocument(scope.url).then(function (pdfDocument) {
-          //  // Document loaded, retrieving the page.
-          //  return pdfDocument.getPage(PAGE_TO_VIEW).then(function (pdfPage) {
-          //    // Creating the page view with default parameters.
-          //    var pdfPageView = new PDFJS.PDFPageView({
-          //      container: container,
-          //      id: PAGE_TO_VIEW,
-          //      scale: SCALE,
-          //      defaultViewport: pdfPage.getViewport(SCALE),
-          //      // We can enable text/annotations layers, if needed
-          //      textLayerFactory: new PDFJS.DefaultTextLayerFactory(),
-          //      annotationsLayerFactory: new PDFJS.DefaultAnnotationsLayerFactory()
-          //    });
-          //    // Associates the actual page with the view, and drawing it
-          //    pdfPageView.setPdfPage(pdfPage);
-          //    return pdfPageView.draw();
-          //  });
-          //});
-          //// --------
-          element.on('mouseup', function(event) {
-            if (scope.onMouseup) {
-              $rootScope.$apply(scope.onMouseup);
-            }
-            // Don't propagate the PDF mouseup event to $document.
-            event.stopPropagation();
-          });
-
-        }
+            //// Loading document.
+            //var PAGE_TO_VIEW = 1;
+            //var SCALE = 1.0;
+            //PDFJS.getDocument(scope.url).then(function (pdfDocument) {
+            //  // Document loaded, retrieving the page.
+            //  return pdfDocument.getPage(PAGE_TO_VIEW).then(function (pdfPage) {
+            //    // Creating the page view with default parameters.
+            //    var pdfPageView = new PDFJS.PDFPageView({
+            //      container: container,
+            //      id: PAGE_TO_VIEW,
+            //      scale: SCALE,
+            //      defaultViewport: pdfPage.getViewport(SCALE),
+            //      // We can enable text/annotations layers, if needed
+            //      textLayerFactory: new PDFJS.DefaultTextLayerFactory(),
+            //      annotationsLayerFactory: new PDFJS.DefaultAnnotationsLayerFactory()
+            //    });
+            //    // Associates the actual page with the view, and drawing it
+            //    pdfPageView.setPdfPage(pdfPage);
+            //    return pdfPageView.draw();
+            //  });
+            //});
+            //// --------
+          }
+        };
       }
     };
-  }]);
+  });
 };
