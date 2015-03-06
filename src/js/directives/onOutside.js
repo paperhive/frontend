1bf514e0135c43341fed9c3ff33f847fe66348bd
André Gaul <andre@gaul.io>
2015-03-06 18:11:43 +0100

    [js] onOutside: parse expression on demand

--- src/js/directives/onOutside.js
+++ src/js/directives/onOutside.js
@@ -12,11 +12,11 @@ module.exports = function (app) {
               // determine if clicked inside the element
               var clickedInside = $.contains(element[0], event.target);
 
-              // get callback that was specified in attribute
-              var callback = $parse(attrs[directiveName]);
-
               // check if event has to be fired
-              if (!clickedInside && callback) {
+              if (!clickedInside) {
+                // get callback that was specified in attribute
+                var callback = $parse(attrs[directiveName]);
+
                 // wrap the call in a scope.$apply to make sure Angular
                 // updates the scope on changes
                 scope.$apply(function () {
