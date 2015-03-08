97978451bb7f3ee17143d1b82fd30757ceb4fb90
Nico Schlömer <nico.schloemer@gmail.com>
2015-03-09 00:40:52 +0100

    move inline editable into its own directive

---
+++ src/js/directives/inline-editable.js
@@ -0,0 +1,21 @@
+module.exports = function (app) {
+
+  app.directive('inlineEditable', function() {
+    return {
+      restrict: 'E',
+      require: 'ngModel',
+      scope: {
+        onSave: '&'
+      },
+      templateUrl: 'templates/directive/inline-editable.html',
+      link: function (scope, element, attrs, ngModelCtrl) {
+        console.log(ngModelCtrl.$modelValue);
+        console.log(ngModelCtrl.$viewValue);
+        scope.c = {
+          isEditMode: false,
+          updatedContent: scope.content
+        };
+      }
+    };
+  });
+};
