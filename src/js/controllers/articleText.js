aaf4e6bba727ed61df68f12c7c39b28c4c4d4a84
André Gaul <andre@gaul.io>
2015-03-11 20:20:26 +0100

    [js,less,html] move margin discussions if there's not enough space

--- src/js/controllers/articleText.js
+++ src/js/controllers/articleText.js
@@ -9,8 +9,66 @@ module.exports = function (app) {
       
       $scope.text = {
         highlightInfos: {},
-        highlightBorder: {}
+        highlightBorder: {},
+        marginDiscussionSizes: {},
+        marginOffsets: {}
       };
+
+      // compute offsets of margin discussions ('boingidi')
+      var updateOffsets = function () {
+        var draftTop = $scope.originalComment.draft.target &&
+          $scope.text.highlightInfos.draft &&
+          $scope.text.highlightInfos.draft.top;
+        var draftHeight = draftTop &&
+          $scope.text.marginDiscussionSizes.draft &&
+          $scope.text.marginDiscussionSizes.draft.height;
+
+        var offsets = _.sortBy(_.compact(_.map(
+          $scope.text.highlightInfos,
+          function (val, key) {
+            return key !== 'draft' ? {id: key, top: val.top} : undefined;
+          })), 'top');
+        console.log(offsets);
+        var currentOffset = 0;
+
+        marginOffsets = {};
+
+        // place draft
+        if (draftTop && draftHeight) {
+          marginOffsets.draft = draftTop;
+        }
+
+        var padding = 5;
+
+        // place all other discussions
+        _.forEach(offsets, function (offset) {
+          var height = $scope.text.marginDiscussionSizes[offset.id] &&
+            $scope.text.marginDiscussionSizes[offset.id].height;
+          if (!offset.top || !height) return;
+
+          // do not fall above previously set offset
+          var top = _.max([offset.top, currentOffset]);
+
+          // if we don't have a draft: just place it to the first possible pos
+          if (!draftTop || !draftHeight) {
+            marginOffsets[offset.id] = top;
+            currentOffset = top + height + padding;
+            return;
+          }
+
+          // fits above draft: place it there
+          if (top + height < draftTop - padding) {
+            marginOffsets[offset.id] = top;
+          // otherwise: place it below draft
+          } else {
+            marginOffsets[offset.id] = _.max([top, draftTop + draftHeight + padding]);
+          }
+          currentOffset = marginOffsets[offset.id] + height + padding;
+        });
+        $scope.text.marginOffsets = marginOffsets;
+      };
+      $scope.$watch('text.highlightInfos', updateOffsets, true);
+      $scope.$watch('text.marginDiscussionSizes', updateOffsets, true);
     }
   ]);
 };
