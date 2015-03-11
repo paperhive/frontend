8839713a330f5a8ec4fc9fb00e7d0b95483ff548
André Gaul <andre@gaul.io>
2015-03-11 15:24:17 +0100

    [js] fix typos

--- src/js/directives/index.js
+++ src/js/directives/index.js
@@ -1,8 +1,8 @@
 module.exports = function (app) {
   require('./annotation')(app);
-  require('./confirmed-clicks')(app);
+  require('./confirmed-click')(app);
   require('./elementSize')(app);
-  require('./gravatars')(app);
+  require('./gravatar')(app);
   require('./highlights')(app);
   require('./marginDiscussion')(app);
   require('./marginDiscussionDraft')(app);
