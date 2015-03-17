7705c9d2146b868090745fa38ffaa4fdcf60c180
André Gaul <andre@gaul.io>
2015-03-17 17:14:18 +0100

    [js] fix some distangle port bug

--- src/js/services/distangle.js
+++ src/js/services/distangle.js
@@ -2,7 +2,7 @@
 module.exports = function (app) {
   app.factory('distangleService', [function () {
     return {
-      distangle: function (anchors, sizes) {
+      distangle: function (anchors, sizes, lb, ub) {
         // JS port of Robert Luce's distangle_fast in
         // https://github.com/rluce/interval-distangle/blob/master/src/distangle.py
         if (anchors.length !== sizes.length) {
@@ -12,9 +12,9 @@ module.exports = function (app) {
         var
           n = anchors.length,
           lsum = new Float64Array(n),
-          asum = Float64Array.from(anchors),
-          span = Float64Array.from(sizes),
-          base = Float64Array.from(anchors),
+          asum = new Float64Array(n),
+          span = new Float64Array(n),
+          base = new Float64Array(n),
           clen = new Int32Array(n),
           backref = new Int32Array(n),
           ic_beg = new Int32Array(n),
@@ -22,8 +22,11 @@ module.exports = function (app) {
           pos = 0,
           ic, i;
 
-        clen.fill(1);
+        asum.set(anchors);
+        span.set(sizes);
+        base.set(anchors);
         for (i=0; i < n; i++) {
+          clen[i] = 1;
           backref[i] = i;
           ic_beg[i] = i;
           ic_end[i] = i+1;
@@ -91,13 +94,18 @@ module.exports = function (app) {
           pos = ic_end[ic];
         }
 
-        // shift
-        offset = 0;
-        for (i = 0; i < n; i++) {
-          if (opt_anchors[i] >= offset) break;
-          opt_anchors[i] = offset;
-          offset += sizes[i];
+        // shift for lower bound
+        if (lb !== undefined) {
+          offset = lb;
+          for (i = 0; i < n; i++) {
+            if (opt_anchors[i] >= offset) break;
+            opt_anchors[i] = offset;
+            offset += sizes[i];
+          }
         }
+
+        // shift for upper bound
+        return opt_anchors;
       }
     };
   }]);
