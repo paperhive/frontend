b239b96d330098484b2343e42b2d2f22325ac2b7
Nico Schlömer <nico.schloemer@gmail.com>
2015-03-20 17:04:54 +0100

    get participants list to work

---
+++ src/js/directives/gravatarList.js
@@ -0,0 +1,35 @@
+'use strict';
+var _ = require('lodash');
+module.exports = function(app) {
+
+  app.directive('gravatarList', function() {
+    return {
+      restrict: 'E',
+      scope: {
+        discussion: '=',
+      },
+      templateUrl: 'templates/directives/gravatarList.html',
+      link: function(scope, element, attrs) {
+        scope.$watch('discussion', function(discussion) {
+          if (discussion !== undefined) {
+
+            scope.participants = _.map(
+              scope.discussion.replies,
+              function(reply) {return reply.author;}
+            );
+            // prepend original annotation author
+            scope.participants.unshift(
+              scope.discussion.originalAnnotation.author
+            );
+
+            // make list unique
+            scope.participants = _.uniq(
+              scope.participants,
+              function(user) {return user._id;}
+            );
+          }
+        });
+      }
+    };
+  });
+};
