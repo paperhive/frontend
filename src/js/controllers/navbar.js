283f3a7fd7e9b32523a797a8826d14e008329bc0
André Gaul <andre@gaul.io>
2015-03-20 20:13:35 +0100

    [js] move search code into separate controller

--- src/js/controllers/navbar.js
+++ src/js/controllers/navbar.js
@@ -1,37 +1,6 @@
 'use strict';
 module.exports = function(app) {
-  app.controller(
-    'NavbarCtrl',
-    ['$scope', '$http', '$location', '$routeSegment', 'config',
-      'notificationService',
-      function(
-        $scope, $http, $location, $routeSegment, config,
-        notificationService
-      ) {
-        $scope.collapsed = true;
-
-        $scope.search = {};
-        $scope.phSearch = function(query, limit) {
-          return $http.get(config.apiUrl + '/articles/', {
-            params: {q: query, limit: limit}
-          })
-          .then(
-            function(response) {return response.data;},
-            function(response) {
-              notificationService.notifications.push({
-                type: 'error',
-                message: 'Could not fetch articles'
-              });
-            }
-          );
-        };
-
-        $scope.goToArticle = function(item, model, label) {
-          $location.path($routeSegment.getSegmentUrl(
-            'articles', {articleId: item._id}
-          ));
-        };
-      }
-    ]
-  );
+  app.controller('NavbarCtrl', ['$scope', function($scope) {
+    $scope.collapsed = true;
+  }]);
 };
