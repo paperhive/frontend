aaf1d6b1d5593d5d965999edc48e3dadf2ef630c
Nico Schlömer <nico.schloemer@gmail.com>
2015-03-31 16:09:03 +0200

    use metaService in pageTitleUpdate

--- src/js/config/pageTitleUpdate.js
+++ src/js/config/pageTitleUpdate.js
@@ -1,27 +1,19 @@
 'use strict';
 module.exports = function(app) {
   app.run([
-    '$rootScope', '$routeSegment',
+    '$rootScope', '$routeSegment', 'metaService',
     function(
-      $rootScope, $routeSegment
+      $rootScope, $routeSegment, metaService
     ) {
-
-      // helper function to set the title
-      $rootScope.page = {
-        setTitle: function(title) {
-          this.title = title;
-        }
-      };
-
       // event listener for title change
       $rootScope.$on('routeSegmentChange', function(event, current, previous) {
-        event.targetScope.$watch('pageTitle', function(value) {
-          $rootScope.page.setTitle(value);
-        });
-        //$rootScope.page.setTitle(
-        //  $routeSegment.chain[$routeSegment.chain.length - 1].params.title ||
-        //  'PaperHub'
-        //);
+        if ($routeSegment.chain &&
+            $routeSegment.chain[$routeSegment.chain.length - 1]
+           ) {
+          metaService.title =
+            $routeSegment.chain[$routeSegment.chain.length - 1].params.title ||
+            'PaperHub';
+        }
       });
     }
   ]);
