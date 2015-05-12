5806388bcf7fe72139d586a3a4f40c9132f97a35
Nico Schlömer <nico.schloemer@gmail.com>
2015-05-12 22:53:27 +0200

    another params fix

--- src/js/config/metaUpdate.js
+++ src/js/config/metaUpdate.js
@@ -12,7 +12,7 @@ module.exports = function(app) {
           $routeSegment.chain[$routeSegment.chain.length - 1].params;
         metaService.set({
           title: (params && params.title) || 'PaperHive',
-          meta: params.meta
+          meta: params && params.meta
         });
       });
     }
