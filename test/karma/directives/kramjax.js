52213ada157e991680aa76b1d43b8a5caaffc948
André Gaul <andre@gaul.io>
2015-07-22 21:55:34 +0200

    [test] add first directive test

---
+++ test/karma/directives/kramjax.js
@@ -0,0 +1,27 @@
+'use strict';
+
+describe('KramJax directive', function() {
+  var $compile;
+  var $rootScope;
+
+  beforeEach(module('paperHive'));
+
+  beforeEach(inject(function(_$compile_, _$rootScope_) {
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+  }));
+
+  it('renders plain text', function() {
+
+    // setup scope
+    var $scope = $rootScope.$new();
+    $scope.kramjaxBody = 'hello world';
+
+    // compile directive with new scope and digest
+    var element = $compile('<kramjax body="kramjaxBody"></kramjax>')($scope);
+    $scope.$digest();
+
+    // check html
+    expect(element.html()).toContain('hello world');
+  });
+});
