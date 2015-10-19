adb860aa4e574ccc0720d0a3460ce5ac9ffde526
André Gaul <andre@gaul.io>
2015-10-19 17:06:08 +0200

    submit email to PaperHive API

--- src/js/controllers/subscribe.js
+++ src/js/controllers/subscribe.js
@@ -2,9 +2,21 @@
 
 module.exports = function(app) {
 
-  app.controller('SubscribeCtrl', ['$scope', '$http',
-    function($scope, $http) {
-      // TODO
+  app.controller('SubscribeCtrl', ['$scope', '$http', 'config',
+    function($scope, $http, config) {
+      $scope.submit = function() {
+        $scope.submitting = true;
+        $scope.error = undefined;
+        $http.post(config.apiUrl + '/newsletter/', {email: $scope.email})
+          .then(function(response) {
+            $scope.submitting = false;
+          }, function(response) {
+            $scope.submitting = false;
+            $scope.error = response.data && response.data.message ||
+              'Unknown error';
+          });
+
+      };
     }
   ]);
 
