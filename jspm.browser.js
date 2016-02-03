7be04027df99717d4a376edc443d9c070a94d057
André Gaul <andre@gaul.io>
2016-02-03 18:47:38 +0100

    use scriptLoad: true instead of rangy override
    see https://github.com/systemjs/systemjs/issues/1075 and
    https://github.com/jspm/registry/pull/733

--- jspm.browser.js
+++ jspm.browser.js
@@ -5,5 +5,10 @@ SystemJS.config({
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*",
     "app/": "app/"
+  },
+  meta: {
+    "*": {
+      scriptLoad: false
+    }
   }
 });
