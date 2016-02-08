63cc782160521850fb3656c2b2d635533f773bbf
Jenny Gebske <jennifer@gebske.de>
2016-02-08 20:54:27 +0100

    add form validation

--- app/controllers/signup.ts
+++ app/controllers/signup.ts
@@ -1,7 +1,7 @@
 'use strict';
 export default function(app) {
-  app.controller('SignupCtrl', ['$scope', '$location', 'authService', 'returnPathService',
-    function($scope, $location, authService, returnPathService) {
+  app.controller('SignupCtrl', ['$scope', '$location', 'authService', 'returnPathService', '$http', 'config',
+    function($scope, $location, authService, returnPathService, $http, config) {
 
       $scope.auth = authService;
       $scope.returnPath = returnPathService;
@@ -11,10 +11,43 @@ export default function(app) {
         password: ''
       };
 
+      $scope.hasError = function(field) {
+        const form = $scope.signupForm;
+        return (form.$submitted || form[field].$touched) &&
+          form[field].$invalid;
+      };
+
+      $scope.$watch('email', function() {
+        $scope.emailError = undefined;
+      });
+
+      $scope.$watch('password', function() {
+        $scope.passwordError = undefined;
+      });
+
       $scope.signup = function() {
-        console.log($scope.signup.email);
-        console.log($scope.signup.password);
-        $location.path($scope.returnPath.returnPath);
+        $scope.subscribing = true;
+        $scope.passwordError = undefined;
+        $scope.emailError = undefined;
+
+        $scope.subscribed = true;
+
+        /*$http.post(config.apiUrl + '/auth/email/initiate/', {
+          email: $scope.signup.email,
+          password: $scope.signup.password,
+          returnUrl: $scope.returnPath.returnPath
+          }).then(function(response) {
+            $scope.subscribing = false;
+            $scope.subscribed = true;
+          }, function(response) {
+            $scope.subscribing = false;
+            // TODO check
+            $scope.passwordError = response.data && response.data.message ||
+              'Unknown error';
+            $scope.emailError = response.data && response.data.message ||
+              'Unknown error';
+          });
+*/
       };
 
     }
