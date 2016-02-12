643056690656c8fa836b83b6e77220e09a3ff54e
Jenny Gebske <jennifer@gebske.de>
2016-02-12 16:13:37 +0100

    check if user is created

--- app/controllers/authReturn.ts
+++ app/controllers/authReturn.ts
@@ -8,6 +8,12 @@ export default function(app) {
         .then(
           function success(data) {
             $location.path($routeParams.returnPath).search({});
+            if ($routeParams.personCreated === "true") {
+              notificationService.notifications.push({
+                type: 'info',
+                message: 'Welcome at PaperHive! Change your settings here'
+              });
+            }
           },
           function fail(reason) {
             $scope.error = reason;
