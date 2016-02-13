f80a82784fcc1fa9a6969aefbb388b9238313255
Jenny Gebske <jennifer@gebske.de>
2016-02-13 21:46:29 +0100

    restructure websocket service

--- app/services/websockets.ts
+++ app/services/websockets.ts
@@ -1,21 +1,36 @@
 'use strict';
+import { clone } from 'lodash';
 import socketio from 'socket.io-client';
+import url from 'url';
 
 export default function(app) {
   app.factory('websocketService', ['config', function(config) {
 
     class WebsocketService {
+      constructor(apiUrl) {
+        // parse url for socketio
+        this.parsedUrl = url.parse(config.apiUrl);
+        this.path = this.parsedUrl.path + '/socket.io';
+      }
 
-      constructor() {
-        console.log('hi there');
-        this.socket = socketio.connect(
-          'https://dev.paperhive.org/documents',
-          {path: '/backend/websockets/socket.io'}
-        );
+      getNamespaceUrl(namespace) {
+        const parsedUrl = clone(this.parsedUrl);
+        parsedUrl.pathname = '/documents';
+        return url.format(parsedUrl);
       }
 
       join(namespace, room) {
-        this.socket.emit('join', {id: '0xACAB'});
+        // connect to socketio endpoint
+        const socket = socketio.connect(
+          this.getNamespaceUrl(namespace),
+          {
+            path: this.path,
+            // TODO: remove for allowing fallback?
+            transports: ['websocket'],
+          }
+        );
+
+        socket.emit('join', {id: '0xACAB'});
       }
 
     }
