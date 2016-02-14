a88c4968cad3a02439d59f0697eb268c19d29ecd
Nico Schlömer <nico.schloemer@gmail.com>
2016-02-14 23:18:21 +0100

    add list of starred documents to user profile

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,7 +1,9 @@
 import gravatar from './gravatar';
 import starButton from './star-button';
+import starredDocs from './starred-docs';
 
 export default function(app) {
   gravatar(app);
   starButton(app);
+  starredDocs(app);
 };
---
+++ app/components/starred-docs.ts
@@ -0,0 +1,46 @@
+'use strict';
+
+export default function(app) {
+    app.component('starredDocs', {
+      bindings: {
+        personId: '<',
+      },
+      controller: [
+        '$scope', '$element', '$attrs', '$http', 'config', 'notificationService',
+        function($scope, $element, $attrs, $http, config, notificationService) {
+          const ctrl = this;
+
+          $scope.$watch('$ctrl.personId', async function(id) {
+            if (!id) { return; }
+            let ret;
+            try {
+              ret = await $http.get(
+                config.apiUrl +
+                  '/people/' + id + '/stars'
+              );
+            } catch (err) {
+              notificationService.notifications.push({
+                type: 'error',
+                message: err.data.message ? err.data.message :
+                  'could not fetch starred documents (unknown reason)'
+              });
+            }
+            ctrl.starredDocuments = ret.data.documents;
+            // This is an async function, so unless we $apply, angular won't
+            // know that values have changed.
+            $scope.$apply();
+          });
+        }],
+        template:
+        `<div class="list-group">
+          <a ng-repeat="doc in $ctrl.starredDocuments" href="./documents/{{doc.id}}" class="list-group-item">
+            <h4 class="list-group-item-heading">{{doc.title}}</h4>
+            <p class="list-group-item-text">
+              <span ng-repeat="author in doc.authors">
+                {{author.name}}{{$last ? '' :', '}}
+              </span>
+            </p>
+          </a>
+        </div>`
+    });
+};
