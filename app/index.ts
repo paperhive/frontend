930cf3de03cd563c09ba911a894b83b777c825c8
Nico Schlömer <nico.schloemer@gmail.com>
2016-02-15 21:11:48 +0100

    move ga code into index.html to avoid linter errors

--- app/index.ts
+++ app/index.ts
@@ -43,15 +43,6 @@ import utils from './utils/index';
 import '../build-tmp/html.js';
 import configJson from '../config.json!json';
 
-// Google Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', configJson.gaTrackingId, 'auto');
-// Commented out as of
-// <https://github.com/angulartics/angulartics-google-analytics#changes-in-the-google-analytics-snippet>.
-// ga('send', 'pageview');
 
 const paperhive = angular
   .module(
