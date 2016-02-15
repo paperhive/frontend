a4763947df71222e1fab0991a38528618e1acc7d
André Gaul <andre@gaul.io>
2016-02-15 10:30:21 +0100

    move WebsocketService class to top-level

--- app/services/websockets.ts
+++ app/services/websockets.ts
@@ -4,56 +4,54 @@ import { Observable } from 'rx';
 import socketio from 'socket.io-client';
 import url from 'url';
 
+class WebsocketService {
+  constructor(apiUrl) {
+    // parse url for socketio
+    this.parsedUrl = url.parse(apiUrl);
+
+    // set socketio path
+    this.path = this.parsedUrl.path;
+    if (this.path[this.path.length - 1] !== '/') {
+      this.path += '/';
+    }
+    this.path += 'socket.io';
+  }
+
+  getNamespaceUrl(namespace) {
+    const parsedUrl = clone(this.parsedUrl);
+    parsedUrl.pathname = '/' + namespace;
+    return url.format(parsedUrl);
+  }
+
+  join(namespace, room) {
+    return Observable.create((observable) => {
+      // connect to socketio endpoint
+      const socket = socketio.connect(
+        this.getNamespaceUrl(namespace),
+        {
+          path: this.path,
+          // TODO: remove for allowing fallback?
+          transports: ['websocket'],
+        }
+      );
+
+      socket.emit('join', {id: room});
+
+      socket.on('update', observable.onNext.bind(observable));
+
+      socket.on('error', observable.onError.bind(observable));
+      socket.on('reconnect_failed', observable.onError.bind(observable));
+      // TODO check if the following errors are fatal
+      // socket.on('reconnect_error', observable.onError.bind(observable));
+      // socket.on('connect_error', observable.onError.bind(observable));
+
+      return () => socket.close();
+    });
+  }
+}
+
 export default function(app) {
   app.factory('websocketService', ['config', function(config) {
-    class WebsocketService {
-      constructor(apiUrl) {
-        // parse url for socketio
-        this.parsedUrl = url.parse(apiUrl);
-
-        // set socketio path
-        this.path = this.parsedUrl.path;
-        if (this.path[this.path.length - 1] !== '/') {
-          this.path += '/';
-        }
-        this.path += 'socket.io';
-      }
-
-      getNamespaceUrl(namespace) {
-        const parsedUrl = clone(this.parsedUrl);
-        parsedUrl.pathname = '/' + namespace;
-        return url.format(parsedUrl);
-      }
-
-      join(namespace, room) {
-        return Observable.create((observable) => {
-          // connect to socketio endpoint
-          const socket = socketio.connect(
-            this.getNamespaceUrl(namespace),
-            {
-              path: this.path,
-              // TODO: remove for allowing fallback?
-              transports: ['websocket'],
-            }
-          );
-
-          socket.emit('join', {id: room});
-
-          socket.on('update', observable.onNext.bind(observable));
-
-          socket.on('error', observable.onError.bind(observable));
-          socket.on('reconnect_failed', observable.onError.bind(observable));
-          // TODO check if the following errors are fatal
-          // socket.on('reconnect_error', observable.onError.bind(observable));
-          // socket.on('connect_error', observable.onError.bind(observable));
-
-          return () => socket.close();
-        });
-      }
-
-    }
-
     return new WebsocketService(config.apiUrl);
-
   }]);
 };
