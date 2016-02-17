88ab2f3b3662301f8a433b465d1ae22f5f5dfd52
André Gaul <andre@gaul.io>
2016-02-17 17:03:43 +0100

    move feedback button left and clean up css

--- app/components/feedback-button.ts
+++ app/components/feedback-button.ts
@@ -1,20 +1,13 @@
 'use strict';
 
 export default function(app) {
-    app.component('feedbackButton', {
-      controller: function() {
-      },
-      template:
-      `<a type="link"
-         class="ph-feedback hidden-xs hidden-sm"
-         target="_blank"
-         ng-href="
-mailto:info@paperhive.org
-?subject=Feedback
-&body=Hi PaperHive,%0D%0A%0D%0A%0D%0A
-"
-         >
-         <i class="fa fa-envelope ph-icon-margin"></i> Feedback
-      </a>`
-    });
+  app.component('feedbackButton', {
+    template:
+    `<a type="link"
+       class="ph-badge-left btn btn-primary btn-sm hidden-xs hidden-sm"
+       ng-href="mailto:info@paperhive.org?subject=Feedback&body=Hi%20PaperHive,%0D%0A%0D%0A%0D%0A"
+       >
+       <i class="fa fa-fw fa-envelope"></i> Feedback
+    </a>`
+  });
 };
