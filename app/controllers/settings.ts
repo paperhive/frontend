45efe3159d8136d0345fd55ee1399272552c6de3
Jenny Gebske <jennifer@gebske.de>
2016-02-23 16:06:00 +0100

    add success/error message

--- app/controllers/settings.ts
+++ app/controllers/settings.ts
@@ -8,13 +8,15 @@ export default function(app) {
       $scope.tab = 'profile';
       $scope.auth = authService;
 
+      $scope.setting = {};
+
       // keep user copy up to date
       $scope.$watch('auth.user', function(user) {
         $scope.user = _.cloneDeep(user);
       });
 
       // sync from orcid
-      $scope.syncFromOrcid = function() {
+      /*$scope.syncFromOrcid = function() {
         $scope.busy = 'sync';
 
         const account = _.findLast($scope.user.externalIds, {type: 'orcid'});
@@ -29,7 +31,7 @@ export default function(app) {
             $scope.busy = false;
             notificationService.httpError('could not sync data');
           });
-      };
+      };*/
 
       // save to api
       $scope.save = function() {
@@ -50,10 +52,16 @@ export default function(app) {
           success(function(data) {
             $scope.busy = false;
             authService.user = data;
+            $scope.setting.succeeded = true;
+            setTimeout(function() {
+              $scope.setting.succeeded = false;
+              $scope.$apply();
+            }, 3000);
           }).
           error(function(data) {
             $scope.busy = false;
             notificationService.httpError('could not save data');
+            $scope.setting.succeeded = false;
           });
       };
     }
