bee7ef1e53e933cc252f6b3af59cd5b0b1a60462
carolinagc <carolina@abriraqui.net>
2016-02-23 09:32:57 +0100

    Add login & homepage e2e tests

---
+++ test/protractor/login.js
@@ -0,0 +1,52 @@
+'use strict';
+
+describe('login page', function() {
+    var form, username;
+    
+    beforeEach(function() {
+	browser.get('/#/login');
+	form = element(by.name('loginForm'));
+    });
+    
+    it('should be present sign in with Google account', function () {
+	var googleLogin = element(by.xpath('/html/body/main/div/div/div[2]/a[1]'));
+	expect(googleLogin.isPresent()).toBe(true);
+
+    });
+
+    it('should be present sign in with ORCID', function () {
+	var orcidLogin = element(by.xpath('/html/body/main/div/div/div[2]/a[2]'));
+	expect(orcidLogin.isPresent()).toBe(true);
+    });
+
+
+    it('should log in if credentials are right', function() {
+	var emailOrUsername = form.element(by.name('emailOrUsername'));
+	var password = form.element(by.name('password'));
+	var loginButton = form.element(by.xpath('/html/body/main/div/div/div[4]/form/div[3]/button'));
+	
+	emailOrUsername.clear();
+	password.clear();
+	emailOrUsername.sendKeys('user');
+	password.sendKeys('secret');
+	loginButton.click();
+
+	// expect(browser.getCurrentUrl()).toBe('SHOULD BE LOGGED IN URL');
+	//	expect($('[ng-class=subscribed]').isDisplayed()).toBeTruthy();
+
+    });
+
+    it('should stay in log in page if credentials does not match', function() {
+	var emailOrUsername = form.element(by.name('emailOrUsername'));
+	var password = form.element(by.name('password'));
+	var loginButton = form.element(by.xpath('/html/body/main/div/div/div[4]/form/div[3]/button'));
+	emailOrUsername.sendKeys('user');
+	loginButton.click();
+
+	expect(browser.getCurrentUrl()).toBe('http://localhost:8080/login');
+
+    });
+
+
+        
+});
--- test/protractor/spec.js
+++ test/protractor/spec.js
@@ -8,4 +8,43 @@ describe('PaperHive homepage', function() {
       'PaperHive · The coworking hub for researchers'
     );
   });
+
+    it('should be present subscribe form', function () {
+	var subscribeForm = element(by.name('subscribeForm'));
+	expect(subscribeForm.isPresent()).toBe(true);
+
+    });
+
+    it('should be present about us link', function () {
+	var aboutUs = element(by.linkText('About us'));
+	expect(aboutUs.isPresent()).toBe(true);
+
+	var linkAboutUs = element(by.xpath('/html/body/footer/ng-include/div[1]/div/div/div[2]/ul/li[1]/a'));
+	linkAboutUs.click();
+	expect(browser.getCurrentUrl()).toBe('http://localhost:8080/about');
+
+    });
+
+    it('should be present Contact link', function () {
+	var contact = element(by.linkText('Contact'));
+	expect(contact.isPresent()).toBe(true);
+
+	var linkContact = element(by.xpath('/html/body/footer/ng-include/div[1]/div/div/div[2]/ul/li[2]/a'));
+	linkContact.click();
+	expect(browser.getCurrentUrl()).toBe('http://localhost:8080/contact');
+
+    });
+
+    it('should be present Legal notice link', function () {
+	var legalNotice = element(by.linkText('Legal notice'));
+	expect(legalNotice.isPresent()).toBe(true);
+
+	var linkLegalNotice = element(by.xpath('/html/body/footer/ng-include/div[1]/div/div/div[2]/ul/li[3]/a'));
+	linkLegalNotice.click();
+	expect(browser.getCurrentUrl()).toBe('http://localhost:8080/legalnotice');
+
+    });
+
+
+
 });
