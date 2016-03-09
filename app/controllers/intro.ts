f3c907779e4e2e2e8e73b896510593300bd01263
Nico Schlömer <nico.schloemer@gmail.com>
2016-03-09 12:12:36 +0100

    slightly different onboarding tour when already logged in

--- app/controllers/intro.ts
+++ app/controllers/intro.ts
@@ -1,8 +1,12 @@
 export default function(app) {
 
   app.controller('IntrojsCtrl', [
-    '$scope',
-    function($scope) {
+    '$scope', 'authService',
+    function($scope, authService) {
+      console.log(authService);
+      console.log(authService.user);
+
+
       $scope.IntroOptions = {
         steps: [
           {
@@ -12,12 +16,12 @@ export default function(app) {
           },
           {
             element: '#introjs-margin-discussion',
-            intro: 'Someone started a discussion. Join by clicking on it and leave a reply!',
+            intro: 'Someone started a discussion. Join by clicking on it and leave a reply! (Login required.)',
             position: 'left'
           },
           {
             element: '#introjs-pdf',
-            intro: 'To start a new discussion, select some text.',
+            intro: 'To start a new discussion, select some text. (Login required.)',
             position: 'top'
           },
           {
@@ -27,7 +31,7 @@ export default function(app) {
           },
           {
             element: '#introjs-hive',
-            intro: 'Hive the article to add it to your collection and receive updates.',
+            intro: 'Hive the article to add it to your collection and receive updates. (Login required.)',
             position: 'left'
           },
           {
@@ -42,8 +46,41 @@ export default function(app) {
         exitOnEsc: true,
         nextLabel: '<strong>Next</strong>',
         prevLabel: 'Previous',
-        skipLabel: 'Exit',
         doneLabel: 'Thanks'
       };
+
+      $scope.auth = authService;
+      // keep user copy up to date
+      $scope.$watch('auth.user', function(user) {
+        if (user) {
+          $scope.IntroOptions.steps = [
+            {
+              element: '#introjs-paperhive-logo',
+              intro: 'Welcome to PaperHive!',
+              position: 'bottom'
+            },
+            {
+              element: '#introjs-margin-discussion',
+              intro: 'Someone started a discussion. Join by clicking on it and leave a reply!',
+              position: 'left'
+            },
+            {
+              element: '#introjs-pdf',
+              intro: 'To start a new discussion, select some text.',
+              position: 'top'
+            },
+            {
+              element: '#srch-term',
+              intro: 'Search for the last article you\'ve read.',
+              position: 'bottom'
+            },
+            {
+              element: '#introjs-hive',
+              intro: 'Hive the article to add it to your collection and receive updates.',
+              position: 'left'
+            },
+          ];
+        }
+      });
     }]);
 };
