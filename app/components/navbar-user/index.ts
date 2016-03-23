989031624a4d87ac6e483c8105ef4abac765cba2
Jenny Gebske <jennifer@gebske.de>
2016-03-23 19:45:15 +0100

    adjust code

--- app/components/navbar-user/index.ts
+++ app/components/navbar-user/index.ts
@@ -4,10 +4,16 @@ import template from './template.html!text';
 
 export default function(app) {
     app.component('navbarUser', {
-      controller: [ '$scope', 'authService', 'tourService',
-        function($scope, authService, tourService) {
+      controller: [ '$scope', 'authService', 'tourService', '$window',
+        function($scope, authService, tourService, $window) {
+
           $scope.auth = authService;
           $scope.tour = tourService;
+
+          $scope.setVisited = function() {
+            $window.localStorage.tourVisited = true;
+          }
+
         }],
       template,
     });
