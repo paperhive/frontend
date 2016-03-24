56a880679f149e5222b6e486454ee6d6000f0d6b
Jenny Gebske <jennifer@gebske.de>
2016-03-24 16:09:36 +0100

    move popover

---
+++ app/directives/onRendered.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+export default function(app) {
+  app.directive('onRendered', ['$parse', '$timeout', function($parse, $timeout) {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        $timeout(() => {
+          const onRendered = $parse(attrs.onRendered);
+          onRendered(scope, {})
+        });
+      }
+    };
+  }]);
+};
