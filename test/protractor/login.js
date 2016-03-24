177798050ef6dd8f19bca7d59b00aae6361d0b21
Nico Schlömer <nico.schloemer@gmail.com>
2016-03-24 12:20:03 +0100

    code cleanup

--- test/protractor/login.js
+++ test/protractor/login.js
@@ -1,46 +1,46 @@
 'use strict';
 
 describe('login page', function() {
-    var form, username;
+  var form, username;
 
-    beforeEach(function () {
-	this.url = browser.baseUrl;
-	browser.get(this.url + '/login');
-	form = element(by.name('loginForm'));
-    });
+  beforeEach(function () {
+    this.url = browser.baseUrl;
+    browser.get(this.url + '/login');
+    form = element(by.name('loginForm'));
+  });
 
-    it('should be present sign in with Google account', function () {
-	var googleLogin = element(by.id('google-login-button'));
-	expect(googleLogin.isPresent()).toBe(true);
+  it('should be present sign in with Google account', function () {
+    var googleLogin = element(by.id('google-login-button'));
+    expect(googleLogin.isPresent()).toBe(true);
 
-    });
+  });
 
-    it('should be present sign in with ORCID', function () {
-	var orcidLogin = element(by.id('orcid-login-button'));
-	expect(orcidLogin.isPresent()).toBe(true);
-    });
+  it('should be present sign in with ORCID', function () {
+    var orcidLogin = element(by.id('orcid-login-button'));
+    expect(orcidLogin.isPresent()).toBe(true);
+  });
 
 
-    it('should log in if credentials are right', function() {
-	var emailOrUsername = form.element(by.name('emailOrUsername'));
-	var password = form.element(by.name('password'));
-  	var emailLoginButton = element(by.id('email-login-button'));   	                                         
-	emailOrUsername.clear();
-	password.clear();
-	emailOrUsername.sendKeys('adalovelace');
-	password.sendKeys('c0d1ng4lifeYO');
-	emailLoginButton.click();
+  it('should log in if credentials are right', function() {
+    var emailOrUsername = form.element(by.name('emailOrUsername'));
+    var password = form.element(by.name('password'));
+    var emailLoginButton = element(by.id('email-login-button'));
+    emailOrUsername.clear();
+    password.clear();
+    emailOrUsername.sendKeys('adalovelace');
+    password.sendKeys('c0d1ng4lifeYO');
+    emailLoginButton.click();
 
-	expect(browser.getCurrentUrl()).toBe(this.url);
-    });
+    expect(browser.getCurrentUrl()).toBe(this.url);
+  });
 
-    it('should stay in log in page if credentials does not match', function() {
-	var emailOrUsername = form.element(by.name('emailOrUsername'));
-	var password = form.element(by.name('password'));
-	var emailLoginButton = element(by.id('email-login-button'));   	                                         
-	emailOrUsername.sendKeys('user');
-	emailLoginButton.click();
+  it('should stay in log in page if credentials does not match', function() {
+    var emailOrUsername = form.element(by.name('emailOrUsername'));
+    var password = form.element(by.name('password'));
+    var emailLoginButton = element(by.id('email-login-button'));
+    emailOrUsername.sendKeys('user');
+    emailLoginButton.click();
 
-	expect(browser.getCurrentUrl()).toBe(this.url + 'login');
-    });
+    expect(browser.getCurrentUrl()).toBe(this.url + 'login');
+  });
 });
--- test/protractor/spec.js
+++ test/protractor/spec.js
@@ -1,12 +1,10 @@
 'use strict';
 
 describe('PaperHive homepage', function() {
-
-
- beforeEach(function () {
-     this.url = browser.baseUrl;
+  beforeEach(function () {
+    this.url = browser.baseUrl;
   });
- 
+
   it('Test page title', function() {
     browser.get(this.url);
     expect(browser.getTitle()).toEqual(
@@ -19,46 +17,40 @@ describe('PaperHive homepage', function() {
     expect(installExtension.isPresent()).toBe(true);
   });
 
-    it('should be present subscribe form', function () {
-	var subscribeForm = element(by.name('subscribeForm'));
-	expect(subscribeForm.isPresent()).toBe(true);
-    });
+  it('should be present subscribe form', function () {
+    var subscribeForm = element(by.name('subscribeForm'));
+    expect(subscribeForm.isPresent()).toBe(true);
+  });
 
-    it('should be present about us link', function () {
+  it('should be present about us link', function () {
+    var aboutUs = element(by.linkText('About us'));
+    expect(aboutUs.isPresent()).toBe(true);
+    var linkAboutUs = element(by.id('about-us-link'));
+    linkAboutUs.click();
+    expect(browser.getCurrentUrl()).toBe(this.url+'about');
+  });
 
-	var aboutUs = element(by.linkText('About us'));
-	expect(aboutUs.isPresent()).toBe(true);
-  var linkAboutUs = element(by.id('about-us-link'));
-	linkAboutUs.click();
+  it('should be present Contact link', function () {
+    var contact = element(by.linkText('Contact'));
+    expect(contact.isPresent()).toBe(true);
+    var linkContact = element(by.id('contact-link'));
+    linkContact.click();
+    expect(browser.getCurrentUrl()).toBe(this.url+'contact');
+  });
 
-	expect(browser.getCurrentUrl()).toBe(this.url+'about');
+  it('should be present Terms of service link', function () {
+    var legalNotice = element(by.linkText('Terms of Service and Privacy Policy'));
+    expect(legalNotice.isPresent()).toBe(true);
+    var linkLegalNotice = element(by.id('terms-link'));
+    linkLegalNotice.click();
+    expect(browser.getCurrentUrl()).toBe(this.url+'terms');
+  });
 
-    });
-
-    it('should be present Contact link', function () {
-	var contact = element(by.linkText('Contact'));
-	expect(contact.isPresent()).toBe(true);
-
-  var linkContact = element(by.id('contact-link'));
-	linkContact.click();
-	expect(browser.getCurrentUrl()).toBe(this.url+'contact');
-    });
-
-    it('should be present Terms of service link', function () {
-	var legalNotice = element(by.linkText('Terms of Service and Privacy Policy'));
-	expect(legalNotice.isPresent()).toBe(true);
-	var linkLegalNotice = element(by.id('terms-link');
-	linkLegalNotice.click();
-	expect(browser.getCurrentUrl()).toBe(this.url+'terms');
-    });
-
-
-    it('should be present Legal notice link', function () {
-	var legalNotice = element(by.linkText('Legal notice'));
-	expect(legalNotice.isPresent()).toBe(true);
-	var linkLegalNotice = element(by.id('legal-link');
-	linkLegalNotice.click();
-	expect(browser.getCurrentUrl()).toBe(this.url + 'legalnotice');
-    });
-
+  it('should be present Legal notice link', function () {
+    var legalNotice = element(by.linkText('Legal notice'));
+    expect(legalNotice.isPresent()).toBe(true);
+    var linkLegalNotice = element(by.id('legal-link'));
+    linkLegalNotice.click();
+    expect(browser.getCurrentUrl()).toBe(this.url + 'legalnotice');
+  });
 });
