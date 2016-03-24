191d2cb0e84d1ecb8531a967a8712546ab038f9f
carolinagc <carolina@abriraqui.net>
2016-03-24 11:42:33 +0100

    Change locator xpath to id

--- test/protractor/login.js
+++ test/protractor/login.js
@@ -11,13 +11,15 @@ describe('login page', function() {
 
     
     it('should be present sign in with Google account', function () {
-	var googleLogin = element(by.xpath('/html/body/main/div/div/div[2]/a[1]'));
+//	var googleLogin = element(by.xpath('/html/body/main/div/div/div[2]/a[1]'));
+	var googleLogin = element(by.id('google-login-button'));
 	expect(googleLogin.isPresent()).toBe(true);
 
     });
 
     it('should be present sign in with ORCID', function () {
-	var orcidLogin = element(by.xpath('/html/body/main/div/div/div[2]/a[2]'));
+//	var orcidLogin = element(by.xpath('/html/body/main/div/div/div[2]/a[2]'));
+	var orcidLogin = element(by.id('orcid-login-button'));
 	expect(orcidLogin.isPresent()).toBe(true);
     });
 
@@ -27,13 +29,13 @@ describe('login page', function() {
 
 	var emailOrUsername = form.element(by.name('emailOrUsername'));
 	var password = form.element(by.name('password'));
-	var loginButton = form.element(by.xpath('/html/body/main/login/div/div/div[4]/form/div[4]/button'));
-     	                                         
+//	var loginButton = form.element(by.xpath('/html/body/main/login/div/div/div[4]/form/div[4]/button'));
+  	var emailLoginButton = element(by.id('email-login-button'));   	                                         
 	emailOrUsername.clear();
 	password.clear();
 	emailOrUsername.sendKeys('adalovelace');
 	password.sendKeys('c0d1ng4lifeYO');
-	loginButton.click();
+	emailLoginButton.click();
 
 	expect(browser.getCurrentUrl()).toBe('https://dev.paperhive.org/frontend/master/');
 
@@ -42,9 +44,10 @@ describe('login page', function() {
     it('should stay in log in page if credentials does not match', function() {
 	var emailOrUsername = form.element(by.name('emailOrUsername'));
 	var password = form.element(by.name('password'));
-	var loginButton = form.element(by.xpath('/html/body/main/div/div/div[4]/form/div[4]/button'));
+//	var loginButton = form.element(by.xpath('/html/body/main/div/div/div[4]/form/div[4]/button'));
+	var emailLoginButton = element(by.id('email-login-button'));   	                                         
 	emailOrUsername.sendKeys('user');
-	loginButton.click();
+	emailLoginButton.click();
 
 	expect(browser.getCurrentUrl()).toBe('http://localhost:8080/login');
 
