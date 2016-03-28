a0de2e03a5eeda5dbaffdc40fe3ce7a3cc356a7e
carolinagc <carolina@abriraqui.net>
2016-03-28 21:43:35 +0200

    Add test when user logged in

---
+++ test/protractor/loggedIn.js
@@ -0,0 +1,78 @@
+'use strict';
+
+describe('logedIn main page', function() {
+    var form, username;
+    
+    beforeEach(function() {
+	this.url = browser.baseUrl;
+	browser.get(this.url + '/login');
+	form = element(by.name('loginForm'));
+
+	var emailOrUsername = form.element(by.name('emailOrUsername'));
+	var password = form.element(by.name('password'));
+	var emailLoginButton = element(by.id('email-login-button'));
+     	                                         
+	emailOrUsername.clear();
+	password.clear();
+	emailOrUsername.sendKeys('adalovelace');
+	password.sendKeys('c0d1ng4lifeYO');
+	emailLoginButton.click();
+
+    });
+    
+    it('should log in', function() {
+	expect(browser.getCurrentUrl()).toBe(this.url);
+    });
+
+    it('should have a user menu', function() {
+	var userMenu = element(by.id('user-menu'));
+	expect(userMenu.isPresent()).toBe(true);
+    });
+
+    it('should have 4 items in user menu', function() {
+	element.all(by.id('user-menu-items li')).then(function(items) {
+	    expect(items.length).toBe(4);
+	    expect(items[0].getText()).toBe('Profile');
+	    expect(items[1].getText()).toBe('Add arXiv article');
+	    expect(items[2].getText()).toBe('Settings');
+	    expect(items[0].getText()).toBe('Log out');
+	});
+    });
+    
+    it('should go to profile page', function() {
+	element.all(by.id('user-menu-items li')).then(function(items) {
+	    var userProfile = items[0];
+	    userProfile.click();
+	    expect(browser.getCurrentUrl).toBe(this.url + 'users/adalovelace');
+	});
+    });
+
+    it('should go to Add document page', function() {
+	element.all(by.id('user-menu-items li')).then(function(items) {
+	    var userAddArXiv = items[1];
+	    userAddArXiv.click();
+	    expect(browser.getCurrentUrl).toBe(this.url + 'documents/new');
+	});
+    });
+
+    it('should go to Settings page', function() {
+	element.all(by.id('user-menu-items li')).then(function(items) {
+	    var userSettings = items[2];
+	    userSettings.click();
+	    expect(browser.getCurrentUrl).toBe(this.url + 'settings');
+	});
+    });
+
+    it('should Log out', function() {
+	element.all(by.id('user-menu-items li')).then(function(items) {
+	    var userLogOut = items[3];
+	    userLogOut.click();
+   	});
+	
+	expect(browser.getCurrentUrl).toBe(this.url);
+	expect(element(by.id('user-menu-items').isPresent(false)));
+
+    });
+
+       
+});
