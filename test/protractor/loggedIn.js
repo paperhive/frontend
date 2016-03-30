f0bebf658dbb59c88dfde14caac1be62edb2f7d8
carolinagc <carolina@abriraqui.net>
2016-03-30 09:58:02 +0200

    Modify test to check user menu list when logged in

--- test/protractor/loggedIn.js
+++ test/protractor/loggedIn.js
@@ -32,8 +32,8 @@ describe('logedIn main page', function() {
     it('should have 4 items in user menu', function() {
 
 //	$('#user-menu-items').findElement(by.linkName('Profile')
-//	element.all(by.css('#user-menu-items li')).then(function(items) {
-	element.all(by.id('user-menu-items li')).then(function(items) {
+	element.all(by.css('#user-menu-items li')).then(function(items) {
+//	element.all(by.id('user-menu-items li')).then(function(items) {
 	    expect(items.length).toBe(4);
 	    expect(items[0].getText()).toBe('Profile');
 	    expect(items[1].getText()).toBe('Add arXiv article');
