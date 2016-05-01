4bc7f36b401528061a5bc0e53d465ca68cc47686
André Gaul <andre@gaul.io>
2016-05-01 23:06:09 +0200

    add start() to tourService and use it in MainPageCtrl

--- app/components/main-page/index.ts
+++ app/components/main-page/index.ts
@@ -3,5 +3,11 @@
 import template from './template.html!text';
 
 export default function(app) {
-  app.component('mainPage', {template});
+  app.component('mainPage', {
+    controller: class MainPageCtrl {
+      static $inject = ['tourService'];
+      constructor(public tourService) {}
+    },
+    template,
+  });
 };
