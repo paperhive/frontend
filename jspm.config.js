6918c86f1c7889c076f835892e166ab6413d1a17
André Gaul <andre@gaul.io>
2016-05-16 08:26:48 +0200

    update angular, angular-bootstrap and async

--- jspm.config.js
+++ jspm.config.js
@@ -24,18 +24,18 @@ SystemJS.config({
     "npm:*.json"
   ],
   map: {
-    "angular": "github:angular/bower-angular@1.5.0",
-    "angular-animate": "github:angular/bower-angular-animate@1.5.0",
-    "angular-bootstrap": "github:angular-ui/bootstrap-bower@1.1.2",
+    "angular": "github:angular/bower-angular@1.5.5",
+    "angular-animate": "github:angular/bower-angular-animate@1.5.5",
+    "angular-bootstrap": "github:angular-ui/bootstrap-bower@1.3.2",
     "angular-leaflet-directive": "github:tombatossals/angular-leaflet-directive@0.10.0",
-    "angular-moment": "npm:angular-moment@1.0.0-beta.3",
-    "angular-route": "github:angular/bower-angular-route@1.5.0",
+    "angular-moment": "npm:angular-moment@1.0.0-beta.6",
+    "angular-route": "github:angular/bower-angular-route@1.5.5",
     "angular-route-segment": "github:artch/angular-route-segment@1.5.0",
-    "angular-sanitize": "github:angular/bower-angular-sanitize@1.5.0",
+    "angular-sanitize": "github:angular/bower-angular-sanitize@1.5.5",
     "angulartics": "npm:angulartics@1.0.3",
     "angulartics-google-analytics": "npm:angulartics-google-analytics@0.1.4",
     "assert": "github:jspm/nodelibs-assert@0.2.0-alpha",
-    "async": "npm:async@2.0.0-rc.3",
+    "async": "npm:async@2.0.0-rc.4",
     "buffer": "github:jspm/nodelibs-buffer@0.2.0-alpha",
     "fs": "github:jspm/nodelibs-fs@0.2.0-alpha",
     "highlightjs": "github:components/highlightjs@9.1.0",
@@ -62,6 +62,21 @@ SystemJS.config({
     "url": "github:jspm/nodelibs-url@0.2.0-alpha"
   },
   packages: {
+    "github:angular/bower-angular-animate@1.5.5": {
+      "map": {
+        "angular": "github:angular/bower-angular@1.5.5"
+      }
+    },
+    "github:angular/bower-angular-route@1.5.5": {
+      "map": {
+        "angular": "github:angular/bower-angular@1.5.5"
+      }
+    },
+    "github:angular/bower-angular-sanitize@1.5.5": {
+      "map": {
+        "angular": "github:angular/bower-angular@1.5.5"
+      }
+    },
     "github:frankwallis/plugin-typescript@4.0.15": {
       "map": {
         "typescript": "npm:typescript@1.8.10"
@@ -75,21 +90,6 @@ SystemJS.config({
     "github:jspm/nodelibs-os@0.2.0-alpha": {
       "map": {
         "os-browserify": "npm:os-browserify@0.2.1"
-      }
-    },
-    "github:angular/bower-angular-animate@1.5.0": {
-      "map": {
-        "angular": "github:angular/bower-angular@1.5.0"
-      }
-    },
-    "github:angular/bower-angular-route@1.5.0": {
-      "map": {
-        "angular": "github:angular/bower-angular@1.5.0"
-      }
-    },
-    "github:angular/bower-angular-sanitize@1.5.0": {
-      "map": {
-        "angular": "github:angular/bower-angular@1.5.0"
       }
     },
     "github:jspm/nodelibs-url@0.2.0-alpha": {
@@ -107,12 +107,12 @@ SystemJS.config({
         "leaflet": "github:Leaflet/Leaflet@0.7.7"
       }
     },
-    "npm:angular-moment@1.0.0-beta.3": {
+    "npm:angular-moment@1.0.0-beta.6": {
       "map": {
         "moment": "npm:moment@2.10.6"
       }
     },
-    "npm:async@2.0.0-rc.3": {
+    "npm:async@2.0.0-rc.4": {
       "map": {
         "lodash": "npm:lodash@4.12.0"
       }
