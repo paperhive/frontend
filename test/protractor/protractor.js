7a465c63963bbd60f8d239373cb9b37c912f9fcf
André Gaul <andre@gaul.io>
2016-05-16 13:37:33 +0200

    fix browser version handling for sauce on demand

--- test/protractor/protractor.js
+++ test/protractor/protractor.js
@@ -17,10 +17,11 @@ if (process.env.SAUCE_ONDEMAND_BROWSERS) {
   exports.config.multiCapabilities = [];
   JSON.parse(process.env.SAUCE_ONDEMAND_BROWSERS).forEach(function(entry) {
     exports.config.multiCapabilities.push({
-      'name': 'PaperHive (' + entry.browser + ')',
-      'browserName': entry.browser,
-      'platform': entry.platform,
-      'build': process.env.BUILD_NUMBER
+      name: 'PaperHive (' + entry.browser + ')',
+      browserName: entry.browser,
+      version: entry['browser-version'],
+      platform: entry.platform,
+      build: process.env.BUILD_NUMBER
     });
     // Test against deployed platform
     exports.config.baseUrl = process.env.TEST_URL;
