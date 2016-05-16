cbeb35c5e0307223ab4a6151f3ac447c88d4161c
André Gaul <andre@gaul.io>
2016-05-16 08:15:52 +0200

    update jspm plugins

--- jspm.config.js
+++ jspm.config.js
@@ -41,7 +41,7 @@ SystemJS.config({
     "highlightjs": "github:components/highlightjs@9.1.0",
     "javascript-detect-element-resize": "github:sdecima/javascript-detect-element-resize@0.5.3",
     "jquery": "npm:jquery@2.2.0",
-    "json": "github:systemjs/plugin-json@0.1.0",
+    "json": "github:systemjs/plugin-json@0.1.2",
     "kramed": "npm:kramed@0.5.5",
     "leaflet": "github:Leaflet/Leaflet@0.7.7",
     "lodash": "npm:lodash@4.12.0",
@@ -50,21 +50,21 @@ SystemJS.config({
     "paperhive-sources": "npm:paperhive-sources@4.0.2",
     "path": "github:jspm/nodelibs-path@0.2.0-alpha",
     "pdfjs-dist": "github:mozilla/pdfjs-dist@1.4.37",
-    "plugin-babel": "npm:systemjs-plugin-babel@0.0.8",
-    "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.2",
+    "plugin-babel": "npm:systemjs-plugin-babel@0.0.10",
+    "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.15",
     "process": "github:jspm/nodelibs-process@0.2.0-alpha",
     "rangy": "github:timdown/rangy-release@1.3.0",
     "readline": "github:jspm/nodelibs-readline@0.2.0-alpha",
     "rx": "npm:rx@4.0.7",
     "socket.io-client": "github:socketio/socket.io-client@1.4.5",
-    "text": "github:systemjs/plugin-text@0.0.7",
-    "ts": "github:frankwallis/plugin-typescript@4.0.2",
+    "text": "github:systemjs/plugin-text@0.0.8",
+    "ts": "github:frankwallis/plugin-typescript@4.0.15",
     "url": "github:jspm/nodelibs-url@0.2.0-alpha"
   },
   packages: {
-    "github:frankwallis/plugin-typescript@4.0.2": {
+    "github:frankwallis/plugin-typescript@4.0.15": {
       "map": {
-        "typescript": "npm:typescript@1.8.9"
+        "typescript": "npm:typescript@1.8.10"
       }
     },
     "github:jspm/nodelibs-buffer@0.2.0-alpha": {
@@ -74,7 +74,7 @@ SystemJS.config({
     },
     "github:jspm/nodelibs-os@0.2.0-alpha": {
       "map": {
-        "os-browserify": "npm:os-browserify@0.2.0"
+        "os-browserify": "npm:os-browserify@0.2.1"
       }
     },
     "github:angular/bower-angular-animate@1.5.0": {
