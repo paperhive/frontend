e55c70ce2b6ce8a51351e1c35964c1df2c8f97ba
Jenny Gebske <jennifer@gebske.de>
2016-05-17 22:54:35 +0200

    add modal

--- app/components/feedback-button.ts
+++ app/components/feedback-button.ts
@@ -2,10 +2,35 @@
 
 export default function(app) {
   app.component('feedbackButton', {
+
+    controller: function($scope, $uibModal, $log) {
+      $scope.open = function () {
+        var modalInstance = $uibModal.open({
+          animation: $scope.animationsEnabled,
+          template:
+            `<div class="modal-header">
+              <h3 class="modal-title">Feedback</h3>
+            </div>
+            <div class="modal-body">
+              <feedback></feedback>
+            </div>
+            <div class="modal-footer">
+              <button class="btn btn-primary" type="button" ng-click="ok()">OK</button>
+              <button class="btn btn-warning" type="button" ng-click="cancel()">Cancel</button>
+            </div>`,
+          resolve: {
+            items: function () {
+              return $scope.items;
+            }
+          }
+        });
+      };
+    },
+
     template:
     `<a type="link"
        class="ph-badge-left btn btn-primary btn-sm hidden-xs hidden-sm"
-       ng-href="mailto:info@paperhive.org?subject=Feedback&body=Hi%20PaperHive,%0D%0A%0D%0A%0D%0A"
+       ng-click="open()"
        >
        <i class="fa fa-fw fa-envelope"></i> Feedback
     </a>`
