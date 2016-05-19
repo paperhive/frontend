0a74f6655d921701ddbb1a745ab2664a4673719f
André Gaul <andre@gaul.io>
2016-05-19 13:59:38 +0200

    fix SecurityError with disabled cookies/localStorage

--- app/services/auth.ts
+++ app/services/auth.ts
@@ -1,4 +1,7 @@
 'use strict';
+
+import {localStorageAvailable} from '../utils/localStorage';
+
 export default function(app) {
   app.factory('authService', ['config', '$http', '$q', '$rootScope', '$window', '$location',
     function(config, $http, $q, $rootScope, $window, $location) {
@@ -79,7 +82,9 @@ export default function(app) {
             authService.token = data.token;
 
             // fires 'storage' event in other tabs
-            $window.localStorage.token = data.token;
+            if (localStorageAvailable) {
+              $window.localStorage.token = data.token;
+            }
 
             // use token for all subsequent HTTP requests to API
             $http.defaults.headers.common['Authorization'] = 'token ' + data.token;
@@ -141,7 +146,9 @@ export default function(app) {
 
       // sign out and forget token
       authService.logout = function() {
-        delete $window.localStorage.token;
+        if (localStorageAvailable) {
+          delete $window.localStorage.token;
+        }
         delete $http.defaults.headers['Authorization'];
         delete authService.user;
         delete authService.token;
@@ -167,9 +174,11 @@ export default function(app) {
         $rootScope.$apply();
       });
 
-      // set token from local storage when initializing (if available)
-      if ($window.localStorage.token) {
-        authService.loginToken($window.localStorage.token);
+      if (localStorageAvailable) {
+        // set token from local storage when initializing (if available)
+        if ($window.localStorage.token) {
+          authService.loginToken($window.localStorage.token);
+        }
       }
 
       return authService;
