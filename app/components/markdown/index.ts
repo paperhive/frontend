e7a76073b452cfe1aebc8dbdb2df710b09db26b1
Jenny Gebske <jennifer@gebske.de>
2016-06-27 13:50:58 +0200

    add textarea and render its input using kramjax

--- app/components/markdown/index.ts
+++ app/components/markdown/index.ts
@@ -3,9 +3,20 @@
 import template from './template.html';
 
 export default function(app) {
-  app.component(
-    'markdown', {
-      template,
-      controller: []}
-  );
+  app.component('markdown', {
+    template,
+    controller: [
+      '$scope',
+      function($scope) {
+        const ctrl = this;
+
+        $scope.text = "It's very easy to make some words **bold** and other words *italic* with Markdown. You can even [link to Google!](http://google.com)";
+
+        $scope.lists = "Sometimes you want numbered lists:";
+
+        $scope.image = "If you want to embed images, this is how you do it: ![Image of Yaktocat](https://octodex.github.com/images/yaktocat.png)";
+
+      }
+    ]
+  });
 };
