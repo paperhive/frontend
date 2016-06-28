35d407ba30462c278baeca40799fa143d97a0a3f
Jenny Gebske <jennifer@gebske.de>
2016-06-28 17:56:25 +0200

    change structure, use subtitles

--- app/components/markdown/index.ts
+++ app/components/markdown/index.ts
@@ -12,39 +12,19 @@ export default function(app) {
 
         ctrl.toc = [];
 
-        ctrl.text = stripIndent(`
+        ctrl.paragraph = stripIndent(`
           Paragraphs are separated by a blank line.
 
+          This is another paragraph.
+        `).trim();
+
+        ctrl.fontStyle = stripIndent(`
           It's very easy to make some words *italic* and other words **bold**.
           Any word wrapped with two tildes (like ~~this~~) will appear crossed out.
+        `).trim();
 
-          A horizontal line looks like this:
-
-          ---
+        ctrl.link = stripIndent(`
           You can even link to [PaperHive](https://paperhive.org/).
-
-        `).trim();
-
-        ctrl.lists = stripIndent(`
-          Bullet list:
-          * Start a line with an asterisk
-          * Food
-            * Fruits
-              * Oranges
-              * Apples
-
-          ***
-
-          Numbered list:
-          1. One
-          2. Two
-          3. Three
-        `).trim();
-
-        ctrl.image = stripIndent(`
-          If you want to embed images, this is how you do it:
-
-          ![Image of Einstein](https://upload.wikimedia.org/wikipedia/commons/6/6f/Einstein-formal_portrait-35.jpg)
         `).trim();
 
         ctrl.headers = stripIndent(`
@@ -55,7 +35,30 @@ export default function(app) {
           ### Another deeper title
 
           You can use one \`#\` all the way up to \`######\` six for different title sizes.
+        `).trim();
 
+        ctrl.lists = stripIndent(`
+          Bullet list:
+          * Start a line with an asterisk
+          * Food
+            * Fruits
+              * Oranges
+              * Apples
+
+          Numbered list:
+          1. One
+          2. Two
+          3. Three
+        `).trim();
+
+        ctrl.separator = stripIndent(`
+          A horizontal line looks like this:
+
+          ---
+          This is text.
+        `).trim();
+
+        ctrl.quote = stripIndent(`
           If you'd like to quote someone, use the > character before the line:
 
           > This is a blockquote.
@@ -63,14 +66,16 @@ export default function(app) {
           > This is the second paragraph.
         `).trim();
 
+        ctrl.image = stripIndent(`
+          If you want to embed images, this is how you do it:
+
+          ![Image of Einstein](https://upload.wikimedia.org/wikipedia/commons/6/6f/Einstein-formal_portrait-35.jpg)
+        `).trim();
+
         ctrl.code = stripIndent(`
-          If you have inline code blocks, you can wrap them in backticks: \`var example = true\`.  If you've got a longer block of code, you can indent with four spaces:
+          If you have inline code blocks, you can wrap them in backticks: \`var example = true\`.
 
-              if (isAwesome){
-                 return true
-              }
-
-          Or you can write multiple lines without indentation by using \`\`\`:
+          If you've got a longer block of code, you can use \`\`\`:
 
           \`\`\`
           if (isAwesome){
