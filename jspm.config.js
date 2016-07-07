1d3264c357cd2af35810371b952686512f27cbef
André Gaul <andre@gaul.io>
2016-07-07 20:58:08 +0200

    update jspm packages

--- jspm.config.js
+++ jspm.config.js
@@ -45,23 +45,26 @@ SystemJS.config({
     "npm:*.json"
   ],
   map: {
+    "Leaflet/Leaflet": "github:Leaflet/Leaflet@0.7.7",
     "angular": "github:angular/bower-angular@1.5.7",
-    "angular-animate": "github:angular/bower-angular-animate@1.5.5",
+    "angular-animate": "github:angular/bower-angular-animate@1.5.7",
     "angular-bootstrap": "github:angular-ui/bootstrap-bower@1.3.3",
     "angular-leaflet-directive": "github:tombatossals/angular-leaflet-directive@0.10.0",
     "angular-moment": "npm:angular-moment@1.0.0-beta.6",
-    "angular-route": "github:angular/bower-angular-route@1.5.5",
-    "angular-route-segment": "github:artch/angular-route-segment@1.5.1",
-    "angular-sanitize": "github:angular/bower-angular-sanitize@1.5.5",
-    "angulartics": "npm:angulartics@1.0.3",
-    "angulartics-google-analytics": "npm:angulartics-google-analytics@0.1.4",
+    "angular-route": "github:angular/bower-angular-route@1.5.7",
+    "angular-route-segment": "npm:angular-route-segment@1.5.1",
+    "angular-sanitize": "github:angular/bower-angular-sanitize@1.5.7",
+    "angulartics": "npm:angulartics@1.1.2",
+    "angulartics-google-analytics": "npm:angulartics-google-analytics@0.2.0",
     "assert": "github:jspm/nodelibs-assert@0.2.0-alpha",
-    "async": "npm:async@2.0.0-rc.5",
+    "async": "npm:async@2.0.0-rc.6",
     "buffer": "github:jspm/nodelibs-buffer@0.2.0-alpha",
+    "components/highlightjs": "github:components/highlightjs@9.4.0",
     "core-js": "npm:core-js@2.4.0",
+    "d-oliveros/ngSmoothScroll": "github:d-oliveros/ngSmoothScroll@2.0.0",
     "dentist": "npm:dentist@1.0.3",
     "fs": "github:jspm/nodelibs-fs@0.2.0-alpha",
-    "highlightjs": "github:components/highlightjs@9.3.0",
+    "highlightjs": "github:components/highlightjs@9.4.0",
     "javascript-detect-element-resize": "github:sdecima/javascript-detect-element-resize@0.5.3",
     "jquery": "npm:jquery@2.2.4",
     "kramed": "npm:kramed@0.5.6",
@@ -71,33 +74,18 @@ SystemJS.config({
     "os": "github:jspm/nodelibs-os@0.2.0-alpha",
     "paperhive-sources": "npm:paperhive-sources@4.0.2",
     "path": "github:jspm/nodelibs-path@0.2.0-alpha",
-    "pdfjs-dist": "github:mozilla/pdfjs-dist@1.5.274",
+    "pdfjs-dist": "github:mozilla/pdfjs-dist@1.5.322",
     "process": "github:jspm/nodelibs-process@0.2.0-alpha",
     "rangy": "github:timdown/rangy-release@1.3.0",
     "readline": "github:jspm/nodelibs-readline@0.2.0-alpha",
     "rx": "npm:rx@4.1.0",
-    "socket.io-client": "github:socketio/socket.io-client@1.4.6",
+    "socket.io-client": "github:socketio/socket.io-client@1.4.8",
     "url": "github:jspm/nodelibs-url@0.2.0-alpha"
   },
   packages: {
-    "github:angular/bower-angular-animate@1.5.5": {
-      "map": {
-        "angular": "github:angular/bower-angular@1.5.7"
-      }
-    },
-    "github:angular/bower-angular-route@1.5.5": {
-      "map": {
-        "angular": "github:angular/bower-angular@1.5.7"
-      }
-    },
-    "github:angular/bower-angular-sanitize@1.5.5": {
-      "map": {
-        "angular": "github:angular/bower-angular@1.5.7"
-      }
-    },
     "github:jspm/nodelibs-buffer@0.2.0-alpha": {
       "map": {
-        "buffer-browserify": "npm:buffer@4.6.0"
+        "buffer-browserify": "npm:buffer@4.7.0"
       }
     },
     "github:jspm/nodelibs-os@0.2.0-alpha": {
@@ -122,25 +110,40 @@ SystemJS.config({
     },
     "npm:angular-moment@1.0.0-beta.6": {
       "map": {
-        "moment": "npm:moment@2.13.0"
-      }
-    },
-    "npm:async@2.0.0-rc.5": {
-      "map": {
-        "lodash": "npm:lodash@4.13.1"
-      }
-    },
-    "npm:buffer@4.6.0": {
-      "map": {
-        "base64-js": "npm:base64-js@1.1.2",
-        "ieee754": "npm:ieee754@1.1.6",
-        "isarray": "npm:isarray@1.0.0"
+        "moment": "npm:moment@2.14.1"
       }
     },
     "npm:url@0.11.0": {
       "map": {
         "punycode": "npm:punycode@1.3.2",
         "querystring": "npm:querystring@0.2.0"
+      }
+    },
+    "npm:async@2.0.0-rc.6": {
+      "map": {
+        "lodash": "npm:lodash@4.13.1"
+      }
+    },
+    "npm:buffer@4.7.0": {
+      "map": {
+        "base64-js": "npm:base64-js@1.1.2",
+        "ieee754": "npm:ieee754@1.1.6",
+        "isarray": "npm:isarray@1.0.0"
+      }
+    },
+    "github:angular/bower-angular-sanitize@1.5.7": {
+      "map": {
+        "angular": "github:angular/bower-angular@1.5.7"
+      }
+    },
+    "github:angular/bower-angular-route@1.5.7": {
+      "map": {
+        "angular": "github:angular/bower-angular@1.5.7"
+      }
+    },
+    "github:angular/bower-angular-animate@1.5.7": {
+      "map": {
+        "angular": "github:angular/bower-angular@1.5.7"
       }
     }
   }
