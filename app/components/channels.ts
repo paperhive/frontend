b27fab2ce5883a743f548f1915be39502876db3b
Jenny Gebske <jennifer@gebske.de>
2016-09-30 15:24:46 +0200

    add channels controller: get channels

--- app/components/channels.ts
+++ app/components/channels.ts
@@ -1,17 +1,30 @@
 'use strict';
 
-import template from './channels.html';
+import template from './channels.html!text';
 
 export default function(app) {
-  app.component(
-    'channels', {
-      template,
-      controller: [
-        '$scope',
-        function($scope) {
-          $scope.channels = {
-            toc: []
-          };
-        }]
-    });
+  app.component('channels', {
+    controller: ['$http', 'authService', 'config', 'notificationService',
+      function($http, authService, config, notificationService) {
+        const ctrl = this;
+        ctrl.auth = authService;
+        ctrl.$onChanges = changesObj => {
+          $http.get(
+            config.apiUrl + '/channels'
+          )
+          .success(ret => {
+            ctrl.channels = ret.channels;
+          })
+          .error(data => {
+            notificationService.notifications.push({
+              type: 'error',
+              message: data.message ? data.message :
+                'could not fetch activities (unknown reason)'
+            });
+          });
+        };
+      }
+    ],
+    template,
+  });
 };
