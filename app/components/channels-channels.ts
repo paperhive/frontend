251207d3bc9e9a2fe3dea280815b6a276f2e9119
Jenny Gebske <jennifer@gebske.de>
2016-10-10 13:46:02 +0200

    get role for every channel

--- app/components/channels-channels.ts
+++ app/components/channels-channels.ts
@@ -1,15 +1,25 @@
 'use strict';
+
+import { find } from 'lodash';
+
 import template from './channels-channels.html';
 
 export default function(app) {
   app.component('channelsChannels', {
     controller: class ChannelsChannelsCtrl {
-      static $inject = ['$location', 'channelService'];
-      constructor(public $location, public channelService) {}
+      static $inject = ['$location', 'authService', 'channelService'];
+      constructor(public $location, public authService, public channelService) {}
 
       openChannel(id) {
         this.$location.path(`/channels/${id}`);
       }
+
+      getMyRoles(channel) {
+        const self = find(channel.members, {person: {id: this.authService.user.id}});
+        if (!self) throw new Error('user not found in members');
+        return self.roles;
+      }
+
     },
     template,
   });
