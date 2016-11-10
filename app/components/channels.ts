ec34be1a9c8ac1c45c8bc04009218f39b3fffda3
Jenny Gebske <jennifer@gebske.de>
2016-11-10 14:10:54 +0100

    replace 'channel new' with modal

--- app/components/channels.ts
+++ app/components/channels.ts
@@ -5,8 +5,18 @@ import template from './channels.html';
 export default function(app) {
   app.component('channels', {
     controller: class ChannelsCtrl {
-      static $inject = ['channelService'];
-      constructor(public channelService) {}
+      static $inject = ['$uibModal', 'channelService'];
+      constructor(public $uibModal, public channelService) {}
+
+      newChannelModalOpen() {
+        this.$uibModal.open({
+          component: 'channelNew',
+          resolve: {
+            redirect: () => true,
+          },
+        });
+      };
+
     },
     template,
   });
