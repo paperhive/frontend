9fd6a0a0fcb1ab5fe4966e3370c995e5c03923cf
André Gaul <andre@gaul.io>
2016-11-18 21:39:28 +0100

    search: move getShortInteger to utils

--- app/components/search-date.ts
+++ app/components/search-date.ts
@@ -2,16 +2,7 @@ import Chartist from 'chartist';
 import { map } from 'lodash';
 
 import template from './search-date.html';
-
-function getShortInteger(value) {
-  const absValue = Math.abs(value);
-  if (absValue < 1e3) return value;
-  if (absValue < 1e6) return `${Math.floor(value * 1e-3)}K`;
-  if (absValue < 1e9) return `${Math.floor(value * 1e-6)}M`;
-  if (absValue < 1e12) return `${Math.floor(value * 1e-9)}G`;
-  if (absValue < 1e15) return `${Math.floor(value * 1e-12)}T`;
-  return value;
-}
+import { getShortInteger } from '../utils/index';
 
 export default function(app) {
   app.component('searchDate', {
--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,6 +1,16 @@
 'use strict';
 import exceptions from './exceptions';
 
+export function getShortInteger(value) {
+  const absValue = Math.abs(value);
+  if (absValue < 1e3) return value;
+  if (absValue < 1e6) return `${Math.floor(value * 1e-3)}K`;
+  if (absValue < 1e9) return `${Math.floor(value * 1e-6)}M`;
+  if (absValue < 1e12) return `${Math.floor(value * 1e-9)}G`;
+  if (absValue < 1e15) return `${Math.floor(value * 1e-12)}T`;
+  return value;
+}
+
 export default function(app) {
   exceptions(app);
 };
