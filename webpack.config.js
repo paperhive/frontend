cfba0f31e98ab1608e2e65beedba314d88154712
André Gaul <andre@gaul.io>
2016-11-21 10:32:30 +0100

    toolchain: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: 'awesome-typescript-loader',
+      },
+    ],
+  },
+};
