27022d304a8fffa012b09754901d2ec3973cc984
Jenny Gebske <jennifer@gebske.de>
2016-12-23 16:04:24 +0100

    adjust types

--- types/srch.d.ts
+++ types/srch.d.ts
@@ -1,6 +1,9 @@
 interface IMap {
   original: number;
   transformed: number;
+  // TODO: parametrize this
+  textObject: PDFTextContentItem;
+  pageNumber: number;
 }
 
 type IMapping = IMap[];
@@ -10,8 +13,14 @@ interface IRange {
   length: number;
 }
 
+interface ITransformedRange {
+  position: number;
+  length: number;
+  transformation: IMap;
+}
+
 declare module 'srch' {
-  export function backTransformRange(positions: number[], mapping: IMapping): number[];
+  export function backTransformRange(positions: number[], mapping: IMapping): ITransformedRange[];
   export class SearchIndex {
     constructor(str: string);
     search(str: string): IRange[];
