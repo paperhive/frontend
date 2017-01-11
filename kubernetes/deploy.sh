813b3003d6a05f0bbc61d668eb24b590eb58fe50
André Gaul <andre@gaul.io>
2017-01-11 14:06:07 +0100

    travis: install kubectl via apt -- "should work™"

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -6,11 +6,10 @@ set -o pipefail
 # is this a pull request build? then don't deploy!
 # NOTE: the decrypted secrets won't be available anyway if the PR comes from
 #       another repository
-if [ "${TRAVIS_PULL_REQUEST}" != "false"]; then
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   echo "This is a pull request. Don't push or deploy."
   exit 0
 fi
-
 
 # sanitize branch name
 export PAPERHIVE_BRANCH="${TRAVIS_BRANCH//[^a-zA-Z0-9\-_]/-}"
@@ -22,9 +21,6 @@ gcloud config set project paperhive-c0ff33
 openssl aes-256-cbc -K $encrypted_90e3403db14e_key -iv $encrypted_90e3403db14e_iv -in gcloud-secret.json.enc -out gcloud-secret.json -d
 gcloud auth activate-service-account --key-file gcloud-secret.json
 gcloud docker -- push ${PAPERHIVE_DOCKER_IMAGE}
-
-# install kubectl
-sudo gcloud -q components install kubectl
 
 # remove old kubernetes resources (if any)
 kubectl --namespace dev delete deployments,services,pods,ingresses -l branch=${PAPERHIVE_BRANCH}
