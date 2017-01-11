64a148279c8c9178605498f0e9b0b969f728fd25
André Gaul <andre@gaul.io>
2017-01-11 12:54:51 +0100

    travis: use deploy script

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -3,13 +3,29 @@
 set -eu
 set -o pipefail
 
-# TODO: use dynamic values
-export PAPERHIVE_BRANCH=test
-export PAPERHIVE_COMMIT=52a2c49c
+# is this a pull request build? then don't deploy!
+if [ "${TRAVIS_PULL_REQUEST}" != "false"]; then
+  echo "This is a pull request. Don't push or deploy."
+  exit 0
+fi
 
+
+# sanitize branch name
+export PAPERHIVE_BRANCH="${TRAVIS_BRANCH//[^a-zA-Z0-9\-_]/-}"
+export PAPERHIVE_COMMIT="${TRAVIS_COMMIT:0:8}"
+export PAPERHIVE_DOCKER_IMAGE="${DOCKER_IMAGE}"
+
+# upload docker image
+gcloud config set project paperhive-c0ff33
+openssl aes-256-cbc -K $encrypted_90e3403db14e_key -iv $encrypted_90e3403db14e_iv -in gcloud-secret.json.enc -out gcloud-secret.json -d
+gcloud auth activate-service-account --key-file gcloud-secret.json
+gcloud docker -- push ${PAPERHIVE_DOCKER_IMAGE}
+
+# remove old kubernetes resources (if any)
 kubectl --namespace dev delete deployments,services,pods,ingresses -l branch=${PAPERHIVE_BRANCH}
 
+# deploy new kubernetes resources
 DIR=$(dirname $0)
 for FILE in ${DIR}/*.yaml; do
-  envsubst '$PAPERHIVE_BRANCH,$PAPERHIVE_COMMIT' < ${FILE} | kubectl create -f -
+  envsubst '$PAPERHIVE_BRANCH,$PAPERHIVE_COMMIT,$PAPERHIVE_DOCKER_IMAGE' < ${FILE} | kubectl create -f -
 done
