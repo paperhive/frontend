73e6a628482fe3fe78f7010680073d1a5abb12d5
André Gaul <andre@gaul.io>
2017-01-11 16:34:11 +0100

    travis: add kubernetes config

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -3,6 +3,8 @@
 set -eu
 set -o pipefail
 
+DIR=$(dirname $0)
+
 # is this a pull request build? then don't deploy!
 # NOTE: the decrypted secrets won't be available anyway if the PR comes from
 #       another repository
@@ -22,11 +24,16 @@ openssl aes-256-cbc -K $encrypted_90e3403db14e_key -iv $encrypted_90e3403db14e_i
 gcloud auth activate-service-account --key-file gcloud-secret.json
 gcloud docker -- push ${PAPERHIVE_DOCKER_IMAGE}
 
+# configure kubectl
+kubectl config set-cluster paperhive-cluster --server=https://104.155.15.140 --api-version=v1 --certificate-authority=${DIR}/ca.crt
+kubectl config set-credentials travis-ci --token=${KUBERNETES_TOKEN}
+kubectl config set-context paperhive-context --cluster=paperhive-cluster --namespace=dev-frontend --user=travis-ci
+kubectl config use-context paperhive-context
+
 # remove old kubernetes resources (if any)
-kubectl --namespace dev delete deployments,services,pods,ingresses -l branch=${PAPERHIVE_BRANCH}
+kubectl delete deployments,services,pods,ingresses -l branch=${PAPERHIVE_BRANCH}
 
 # deploy new kubernetes resources
-DIR=$(dirname $0)
-for FILE in ${DIR}/*.yaml; do
+for FILE in ${DIR}/frontend-*.yaml; do
   envsubst '$PAPERHIVE_BRANCH,$PAPERHIVE_COMMIT,$PAPERHIVE_DOCKER_IMAGE' < ${FILE} | kubectl create -f -
 done
