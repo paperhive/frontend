e1a924f92badcc5341804bc027b6630ecfe18b2e
André Gaul <andre@gaul.io>
2017-03-01 21:46:29 +0100

    config: remove fa-spin from animated classes

--- app/config/animate.ts
+++ app/config/animate.ts
@@ -2,6 +2,6 @@ export default function(app) {
   app.config(['$animateProvider', function($animateProvider) {
     // filter font awesome animated icons
     // http://stackoverflow.com/a/24633065/1219479
-    $animateProvider.classNameFilter(/^((?!(fa-spinner)).)*$/);
+    $animateProvider.classNameFilter(/^((?!(fa-spin)).)*$/);
   }]);
 }
