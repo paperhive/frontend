f4b2e207d350b103271028a76179bf424d90e28c
André Gaul <andre@gaul.io>
2017-03-29 18:49:42 +0200

    component(document-sidenav): use pagination in small sidenav

--- app/components/document-sidenav-pagination.ts
+++ app/components/document-sidenav-pagination.ts
@@ -1,19 +1,31 @@
+require('./document-sidenav-pagination.less');
+
 export default function(app) {
   app.component('documentSidenavPagination', {
     bindings: {
+      large: '<',
       pageNumber: '<',
       totalPageNumber: '<',
       onChange: '&',
     },
     controller: class DocumentSidenavPaginationCtrl {
       pageNumber: number;
-      pageNumberModel: number;
+      pageNumberModel: string;
       totalPageNumber: number;
       onChange: (o: {pageNumber: number}) => Promise<void>;
 
       static $inject = ['$scope'];
       constructor(public $scope) {
         $scope.$watch('$ctrl.pageNumber', pageNumber => this.pageNumberModel = pageNumber);
+      }
+
+      submit() {
+        const pageNumber = parseInt(this.pageNumberModel, 10);
+        if (isNaN(pageNumber)) throw new Error('page number is not an integer');
+        if (pageNumber < 1 || pageNumber > this.totalPageNumber) {
+          throw new Error('page number out of range');
+        }
+        this.onChange({pageNumber});
       }
     },
     template: require('./document-sidenav-pagination.html'),
