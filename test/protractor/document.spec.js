13528b8065332a8098b91110d6d469ad73628aec
André Gaul <andre@gaul.io>
2017-04-21 09:34:31 +0200

    Revert "test: temporarily disable document tests"
    This reverts commit 5cc90cd082246199fc899e553d8e8f12ce0d9031.

--- test/protractor/document.spec.js
+++ test/protractor/document.spec.js
@@ -8,7 +8,7 @@ describe('document N2iCMzBfgu3p', function() {
     );
   });
 
-  xit('should scroll to anchor J4zEJCRoRyA2', function() {
+  it('should scroll to anchor J4zEJCRoRyA2', function() {
     browser.get('./documents/N2iCMzBfgu3p?a=s:J4zEJCRoRyA2');
     // anchor is on page 25 so make sure it is present and
     // in the viewport eventually
@@ -16,7 +16,7 @@ describe('document N2iCMzBfgu3p', function() {
     browser.wait(() => utils.isInViewport(element(by.id('p:25'))), 5000);
   });
 
-  xit('should scroll to discussion UfRBMhjOLa42', () => {
+  it('should scroll to discussion UfRBMhjOLa42', () => {
     browser.get('./documents/N2iCMzBfgu3p?a=d:UfRBMhjOLa42');
     // discussion is on page 26 so make sure it is present and
     // in the viewport eventually
