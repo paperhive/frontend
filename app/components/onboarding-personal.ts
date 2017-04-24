5d6b518f6d4f8911b08ada4bb3e20e8569967d0b
André Gaul <andre@gaul.io>
2017-04-24 23:47:01 +0200

    component(onboarding-personal): save name, discipline and occupation

--- app/components/onboarding-personal.ts
+++ app/components/onboarding-personal.ts
@@ -1,3 +1,5 @@
+import { cloneDeep } from 'lodash';
+
 require('./onboarding.less');
 
 export default function(app) {
@@ -56,20 +58,42 @@ export default function(app) {
       occupationSelect: string;
       occupationText: string;
 
+      submitting = false;
       complete = false;
 
-      static $inject = ['$scope', 'authService'];
-      constructor($scope, public authService) {
+      displayName: string;
+      email: string;
+
+      static $inject = ['$scope', 'authService', 'personService'];
+      constructor($scope, public authService, public personService) {
         this.disciplineOptions = [...this.disciplines, 'Other'];
         this.occupationOptions = [...this.occupations, 'Other'];
 
+        $scope.$watch('$ctrl.authService.user.displayName', displayName => this.displayName = displayName);
         $scope.$watch('$ctrl.authService.user.discipline', this.updateDiscipline.bind(this));
         $scope.$watch('$ctrl.authService.user.occupation', this.updateOccupation.bind(this));
       }
 
       next() {
-        this.complete = true;
-        this.onNext();
+        this.submitting = true;
+
+        // TODO: email
+
+        const person = cloneDeep(this.authService.user);
+        person.displayName = this.displayName;
+        person.discipline = this.disciplineSelect !== 'Other'
+          ? this.disciplineSelect : this.disciplineText;
+        person.occupation = this.occupationSelect !== 'Other'
+          ? this.occupationSelect : this.occupationText;
+
+        this.personService.update(person)
+          .then(() => {
+            this.complete = true;
+            this.onNext();
+          })
+          .finally(() => {
+            this.submitting = false;
+          });
       }
 
       updateDiscipline(discipline) {
