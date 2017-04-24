0eee25b7c766affeb217597a689ac220f481d664
André Gaul <andre@gaul.io>
2017-04-24 10:53:19 +0200

    component(onboarding): scroll to current step

--- app/components/onboarding.ts
+++ app/components/onboarding.ts
@@ -5,12 +5,26 @@ export default function(app) {
     controller: class OnboardingCtrl {
       currentStep = 1;
 
+      static $inject = ['scroll'];
+      constructor(public scroll) {}
+
       next() {
         this.currentStep += 1;
+        this.scrollToCurrentStep();
       }
 
       previous() {
         this.currentStep -= 1;
+        this.scrollToCurrentStep();
+      }
+
+      scrollToCurrentStep() {
+        const stepElements = [
+          'onboarding-personal',
+          'onboarding-channel',
+          'onboarding-bookmark',
+        ];
+        this.scroll.scrollTo(stepElements[this.currentStep - 1], {offset: 30});
       }
     },
     template: require('./onboarding.html'),
