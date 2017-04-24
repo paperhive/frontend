9c90a2209f7893a61e4db214039b6087686809e5
André Gaul <andre@gaul.io>
2017-04-24 20:55:15 +0200

    component(onboarding-personal): add disciplines and occupations

--- app/components/onboarding-personal.ts
+++ app/components/onboarding-personal.ts
@@ -6,7 +6,91 @@ export default function(app) {
       active: '<',
       onNext: '&',
     },
-    controller: class OnboardingPersonalCtrl {},
+    controller: class OnboardingPersonalCtrl {
+      const disciplines = [
+        'Agricultural Sciences',
+        'Architecture',
+        'Biochemistry',
+        'Biology',
+        'Business and Management',
+        'Chemistry',
+        'Computer Science',
+        'Dentistry',
+        'Earth Sciences',
+        'Economics and Finance',
+        'Engineering',
+        'Environmental Science',
+        'Graphic Design',
+        'History',
+        'Law',
+        'Linguistics',
+        'Literature',
+        'Materials Science',
+        'Mathematics',
+        'Medicine',
+        'Microbiology',
+        'Musicology',
+        'Neuroscience',
+        'Nursing',
+        'Pharmacology',
+        'Philosophy',
+        'Physics',
+        'Psychology',
+        'Social Sciences',
+        'Sports Science',
+        'Veterinary Medicine',
+      ];
+      disciplineOptions: string[];
+      disciplineSelect: string;
+      disciplineText: string;
+
+      const occupations = [
+        'Professor',
+        'Postdoc',
+        'PhD student',
+        'Student',
+      ];
+      occupationOptions: string[];
+      occupationSelect: string;
+      occupationText: string;
+
+      static $inject = ['$scope', 'authService'];
+      constructor($scope, public authService) {
+        this.disciplineOptions = [...this.disciplines, 'Other'];
+        this.occupationOptions = [...this.occupations, 'Other'];
+
+        $scope.$watch('$ctrl.authService.user.discipline', this.updateDiscipline.bind(this));
+        $scope.$watch('$ctrl.authService.user.occupation', this.updateOccupation.bind(this));
+      }
+
+      updateDiscipline(discipline) {
+        this.disciplineText = undefined;
+        if (!discipline) {
+          this.disciplineSelect = '';
+          return;
+        }
+        if (this.disciplines.indexOf(discipline) !== -1) {
+          this.disciplineSelect = discipline;
+          return;
+        }
+        this.disciplineSelect = 'Other';
+        this.disciplineText = discipline;
+      }
+
+      updateOccupation(occupation) {
+        this.occupationText = undefined;
+        if (!occupation) {
+          this.occupationSelect = '';
+          return;
+        }
+        if (this.occupations.indexOf(occupation) !== -1) {
+          this.occupationSelect = occupation;
+          return;
+        }
+        this.occupationSelect = 'Other';
+        this.occupationText = occupation;
+      }
+    },
     template: require('./onboarding-personal.html'),
   });
 };
