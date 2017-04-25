a0d348ee30ec613463a3f5575a30663fe7f8aa58
André Gaul <andre@gaul.io>
2017-04-25 23:57:09 +0200

    component(onboarding-channel): add channel and member emails

--- app/components/onboarding-channel.ts
+++ app/components/onboarding-channel.ts
@@ -9,11 +9,50 @@ export default function(app) {
     },
     controller: class OnboardingChannelCtrl {
       onNext: () => void;
+
+      name: string;
+      email: string;
+      emails: string[] = [];
+
       complete = false;
+      submitting = false;
+
+      static $inject = ['channelService'];
+      constructor(public channelService) {}
+
+      add(email) {
+        const sanitizedEmail = OnboardingChannelCtrl.sanitizeEmail(email);
+        if (this.emails.indexOf(sanitizedEmail) !== -1) return;
+        this.emails.push(sanitizedEmail);
+        this.email = undefined;
+      }
+
+      remove(email) {
+        const index = this.emails.indexOf(OnboardingChannelCtrl.sanitizeEmail(email));
+        if (index === -1) return;
+        this.emails.splice(index, 1);
+      }
+
+      static sanitizeEmail(email: string) {
+        return email.toLowerCase().trim();
+      }
 
       next() {
-        this.complete = true;
-        this.onNext();
+        this.submitting = true;
+        this.channelService.create({name: this.name})
+          .then(channel => {
+            return Promise.all(this.emails.map(email => {
+              return this.channelService.invitationCreate(channel.id, {
+                email,
+                roles: ['member'],
+              });
+            }));
+          })
+          .then(() => {
+            this.complete = true;
+            this.onNext();
+          })
+          .finally(() => this.submitting = false);
       }
     },
     template: require('./onboarding-channel.html'),
