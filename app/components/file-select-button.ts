d5ce35aa6b27490edb1c2ec42b7cafd9bf8f5eb2
André Gaul <andre@gaul.io>
2017-11-21 23:06:22 +0100

    file-select-button: add drop zone

--- app/components/file-select-button.ts
+++ app/components/file-select-button.ts
@@ -10,17 +10,54 @@ export default function(app: IModule) {
       onSelect: '&',
     },
     controller: class FileSelectButtonCtrl {
+      public dragActive: boolean;
       public onSelect: (o: {file: File}) => void;
 
       static $inject = ['$element', '$scope'];
       constructor(public $element, $scope) {
+        // register handler for file dialog change
         $element.find('> label > input')
-          .on('change', event => $scope.$apply(() => this.select(event)));
+          .on('change', event => $scope.$apply(() => this.onFileDialogSelect(event)));
+
+        // define drop zone
+        let enterCount = 0;
+        $element.on('dragenter', event => {
+          event.preventDefault();
+          if (enterCount === 0) {
+            $scope.$apply(() => this.dragActive = true);
+          }
+          enterCount++;
+        });
+        $element.on('dragover', event => event.preventDefault());
+        $element.on('dragleave', event => {
+          enterCount--;
+          if (enterCount === 0) {
+            $scope.$apply(() => this.dragActive = false);
+          }
+        });
+
+        // register handler for file drop
+        $element.on('drop', event => {
+          event.preventDefault();
+          $scope.$apply(() => this.onDrop(event));
+        });
       }
 
-      protected select(event) {
+      protected onFileDialogSelect(event) {
         const files = event.target.files;
         if (!files || files.length !== 1) {
+          this.onSelect(undefined);
+          return;
+        }
+        this.onSelect({file: files[0]});
+      }
+
+      protected onDrop(event) {
+        event.preventDefault();
+        this.dragActive = false;
+
+        const files = event.originalEvent.dataTransfer.files;
+        if (!files || files.length === 0) {
           this.onSelect(undefined);
           return;
         }
