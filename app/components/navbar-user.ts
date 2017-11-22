0cf026cc0d220228f42ea2705d931d3a0ba36f14
André Gaul <andre@gaul.io>
2017-11-22 19:58:01 +0100

    navbar-user: add document upload

--- app/components/navbar-user.ts
+++ app/components/navbar-user.ts
@@ -1,8 +1,8 @@
 export default function(app) {
   app.component('navbarUser', {
     controller: class NavbarUserCtrl {
-      static $inject = ['authService'];
-      constructor(public authService) {}
+      static $inject = ['authService', 'documentUploadModalService'];
+      constructor(public authService, public documentUploadModalService) {}
     },
     template: require('./navbar-user.html'),
   });
