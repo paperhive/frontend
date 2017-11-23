24352179a170606f5ba1ebe5a029df544c8a0c02
André Gaul <andre@gaul.io>
2017-11-23 17:31:12 +0100

    navbar-user: add feature flag for uploads

--- app/components/navbar-user.ts
+++ app/components/navbar-user.ts
@@ -1,8 +1,8 @@
 export default function(app) {
   app.component('navbarUser', {
     controller: class NavbarUserCtrl {
-      static $inject = ['authService', 'documentUploadModalService'];
-      constructor(public authService, public documentUploadModalService) {}
+      static $inject = ['authService', 'documentUploadModalService', 'featureFlagsService'];
+      constructor(public authService, public documentUploadModalService, public featureFlagsService) {}
     },
     template: require('./navbar-user.html'),
   });
