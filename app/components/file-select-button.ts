e1bbf8f97cfa112b4034bc6b2590e0237ad80f46
André Gaul <andre@gaul.io>
2017-11-23 16:11:36 +0100

    file-select-button: fix some edge cases (selecting via dropzone/button)

--- app/components/file-select-button.ts
+++ app/components/file-select-button.ts
@@ -10,27 +10,44 @@ export default function(app: IModule) {
       onSelect: '&',
     },
     controller: class FileSelectButtonCtrl {
+      public disabled: boolean;
       public dragActive: boolean;
       public onSelect: (o: {file: File}) => void;
+
+      protected fileElement: JQuery;
+      protected onFileDialogChange: (e: Event) => void;
 
       static $inject = ['$element', '$scope'];
       constructor(public $element, $scope) {
         // register handler for file dialog change
-        $element.find('> label > input')
-          .on('change', event => $scope.$apply(() => this.onFileDialogSelect(event)));
+        this.onFileDialogChange = event => $scope.$apply(() => this.onFileDialogSelect(event));
+        this.fileElement = $element.find('> label > input');
+        this.fileElement.on('change', this.onFileDialogChange);
 
-        // define drop zone
+        // counts dragenter events (fires multiple time due to nested elements)
         let enterCount = 0;
+
+        // calling preventDefault() signals a drop zone
         $element.on('dragenter', event => {
-          event.preventDefault();
-          if (enterCount === 0) {
-            $scope.$apply(() => this.dragActive = true);
-          }
           enterCount++;
+
+          if (this.disabled) return;
+
+          event.preventDefault();
+          $scope.$apply(() => this.dragActive = true);
         });
-        $element.on('dragover', event => event.preventDefault());
+
+        // calling preventDefault() signals a drop zone
+        $element.on('dragover', event => {
+          if (this.disabled) return;
+
+          event.preventDefault();
+        });
+
+        // restore dragActive when done
         $element.on('dragleave', event => {
           enterCount--;
+
           if (enterCount === 0) {
             $scope.$apply(() => this.dragActive = false);
           }
@@ -39,23 +56,37 @@ export default function(app: IModule) {
         // register handler for file drop
         $element.on('drop', event => {
           enterCount = 0;
+          $scope.$apply(() => this.dragActive = false);
+
+          if (this.disabled) return;
+
           event.preventDefault();
           $scope.$apply(() => this.onDrop(event));
         });
       }
 
       protected onFileDialogSelect(event) {
+        if (this.disabled) return;
+
         const files = event.target.files;
+
         if (!files || files.length !== 1) {
           this.onSelect(undefined);
           return;
         }
         this.onSelect({file: files[0]});
+
+        // reset file input element for enabling selecting the same file
+        // (in case you selected another file in the meantime by dropping it)
+        this.fileElement.off('change', this.onFileDialogChange);
+        event.target.value = '';
+        this.fileElement.on('change', this.onFileDialogChange);
       }
 
       protected onDrop(event) {
-        event.preventDefault();
         this.dragActive = false;
+
+        if (this.disabled) return;
 
         const files = event.originalEvent.dataTransfer.files;
         if (!files || files.length === 0) {
