9de1852acd688b081bd22b000f76222be5edb8a6
André Gaul <andre@gaul.io>
2018-03-25 20:02:49 +0200

    navbar-search: use new document-items api

--- app/components/navbar-search.ts
+++ app/components/navbar-search.ts
@@ -1,12 +1,14 @@
+import { isDocumentItemSharedWithUser } from '../utils/document-items';
+
 const navbarSearchItemUrl = require('!ngtemplate-loader?relativeTo=/app!html-loader!./navbar-search-item.html');
 
 export default function(app) {
   app.component('navbarSearch', {
-    controller: ['$scope', '$http', '$location', '$routeSegment', 'config',
-      'notificationService',
+    controller: ['$scope', '$http', '$location', '$routeSegment',
+      'authService', 'config', 'notificationService',
       function(
-        $scope, $http, $location, $routeSegment, config,
-        notificationService,
+        $scope, $http, $location, $routeSegment,
+        authService, config, notificationService,
       ) {
         $scope.navbarSearchItemUrl = navbarSearchItemUrl;
 
@@ -16,12 +18,12 @@ export default function(app) {
 
         $scope.search = {};
         $scope.phSearch = function(query, limit) {
-          return $http.get(config.apiUrl + '/documents/prefix_search', {
-            params: {q: query, limit},
+          return $http.get(config.apiUrl + '/document-items/search/by-prefix', {
+            params: {prefix: query, limit},
           })
           .then(
             function(response) {
-              return response.data.documents;
+              return response.data.hits;
             },
             function(response) {
               notificationService.notifications.push({
@@ -31,6 +33,9 @@ export default function(app) {
             },
           );
         };
+
+        $scope.isOwnedByYou = documentItem => authService.user && documentItem.owner === authService.user.id;
+        $scope.isSharedWithYou = documentItem => isDocumentItemSharedWithUser(documentItem, authService.user);
 
         $scope.goToDocument = function(item, model, label) {
           $location
