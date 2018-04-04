365a9cd2b228ecce6cf09bf0a9de4ac13b020545
André Gaul <andre@gaul.io>
2018-04-04 23:10:31 +0200

    document-upload-modal: redirect to document item after successful upload

--- app/services/document-upload-modal.ts
+++ app/services/document-upload-modal.ts
@@ -2,29 +2,41 @@ import { IModule } from 'angular';
 
 export default function(app: IModule) {
   app.service('documentUploadModalService', class DocumentUploadModalService {
-    static $inject = ['$uibModal'];
+    static $inject = ['$location', '$routeSegment', '$uibModal'];
     modalConfig = {
       animation: true,
       backdrop: 'static',
       template: `
         <document-upload-modal
-          on-cancel="$ctrl.close()"
+          on-cancel="$ctrl.dismiss()"
           on-uploaded="$ctrl.close(result)"
         ></document-upload-modal>`,
       controllerAs: '$ctrl',
       controller: class ModalCtrl {
         static $inject = ['$uibModalInstance'];
         constructor(public $uibModalInstance) {}
-        close(result) {
-          this.$uibModalInstance.close(result);
+
+        dismiss() {
+          this.$uibModalInstance.dismiss();
+        }
+
+        close(documentItem) {
+          this.$uibModalInstance.close({documentItem});
         }
       },
     };
 
-    constructor(public $uibModal) {}
+    constructor(public $location, public $routeSegment, public $uibModal) {}
 
     open() {
-      return this.$uibModal.open(this.modalConfig);
+      return this.$uibModal
+        .open(this.modalConfig)
+        .result
+        .then(({documentItem}) => {
+          const url = this.$routeSegment.getSegmentUrl('documentItem', {documentItem: documentItem.id});
+          this.$location.url(url);
+        })
+        .catch(() => { /* ignore error */ });
     }
   });
 }
