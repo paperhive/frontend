f87f8610d80e8c0fa879e77222738869e3a17dbd
André Gaul <andre@gaul.io>
2018-04-05 15:04:20 +0200

    test: use document items

--- test/protractor/document.spec.js
+++ test/protractor/document.spec.js
@@ -1,15 +1,15 @@
 const utils = require('./utils');
 
-describe('document N2iCMzBfgu3p', function() {
+describe('document item ', function() {
   it('should load and set the title', function() {
-    browser.get('./documents/N2iCMzBfgu3p');
+    browser.get('./documents/items/82PV71fhWSf7');
     expect(browser.getTitle()).toEqual(
       'Preconditioned Recycling Krylov subspace methods for self-adjoint problems · PaperHive'
     );
   });
 
   it('should scroll to anchor J4zEJCRoRyA2', function() {
-    browser.get('./documents/N2iCMzBfgu3p?a=s:J4zEJCRoRyA2');
+    browser.get('./documents/items/82PV71fhWSf7?a=s:J4zEJCRoRyA2');
     // anchor is on page 25 so make sure it is present and
     // in the viewport eventually
     browser.wait(() => element(by.id('p:25')).isPresent(), 20000);
@@ -17,7 +17,7 @@ describe('document N2iCMzBfgu3p', function() {
   });
 
   it('should scroll to discussion UfRBMhjOLa42', () => {
-    browser.get('./documents/N2iCMzBfgu3p?a=d:UfRBMhjOLa42');
+    browser.get('./documents/items/82PV71fhWSf7?a=d:UfRBMhjOLa42');
     // discussion is on page 26 so make sure it is present and
     // in the viewport eventually
     browser.wait(() => element(by.id('p:26')).isPresent(), 20000);
