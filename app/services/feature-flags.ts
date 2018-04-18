bcabb43f5a2a5c648f57357b12343e867321d10f
André Gaul <andre@gaul.io>
2018-04-18 15:37:18 +0200

    feature-flags: set flags from account

--- app/services/feature-flags.ts
+++ app/services/feature-flags.ts
@@ -2,12 +2,14 @@ import { ILocationService, IRootScopeService, IWindowService } from 'angular';
 
 export default function(app) {
   type Flag = 'uploads' /* | 'feature1' | 'feature2' */;
+  type Flags = {[k in Flag]?: boolean};
 
   app.service('featureFlagsService', class FeatureFlags {
-    public flags: {[k in Flag]?: boolean} = {};
+    public flags: Flags = {};
 
-    static $inject = ['$location', '$rootScope', '$window'];
-    constructor($location: ILocationService, protected $rootScope: IRootScopeService, $window: IWindowService) {
+    static $inject = ['$location', '$rootScope', '$window', 'authService'];
+    constructor($location: ILocationService, protected $rootScope: IRootScopeService,
+                $window: IWindowService, authService: any) {
       // enable feature flags from URL query string (example: ?enableFeature=uploads)
       const enableFeature = $location.search().enableFeature;
       if (enableFeature !== undefined) {
@@ -19,6 +21,8 @@ export default function(app) {
       // expose {enable,disable}Feature on window for testing
       $window.enableFeature = flag => this.enable(flag);
       $window.disableFeature = flag => this.disable(flag);
+
+      $rootScope.$watch(() => authService.user, (user) => this.updateFromUser(user));
     }
 
     public disable(flag: Flag) {
@@ -35,6 +39,12 @@ export default function(app) {
 
     public set(flag: Flag, enabled: boolean) {
       this.$rootScope.$apply(() => this.flags[flag] = enabled);
+    }
+
+    protected updateFromUser(user) {
+      const featureFlags = user && user.account && user.account.featureFlags as Flags;
+      if (!featureFlags) return;
+      Object.entries(featureFlags).forEach(([flag, enabled]) => this.flags[flag] = enabled);
     }
   });
 }
