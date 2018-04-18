1de5189e245f85252f1b62a7411a526091a57f06
André Gaul <andre@gaul.io>
2018-04-18 22:20:30 +0200

    document-new: adapt to document items

--- app/components/document-new.ts
+++ app/components/document-new.ts
@@ -5,29 +5,22 @@ export default function(app) {
 
       error: string;
       submitting: boolean;
-      static $inject = ['$http', '$location', 'config'];
-      constructor(public $http, public $location, public config) {
-        this.type = 'url';
+      static $inject = ['$http', '$location', 'documentItemsApi'];
+      constructor(public $http, public $location, public documentItemsApi) {
+        this.type = $location.search().type || 'crossref';
       }
 
       submit(type, id) {
         this.submitting = true;
         this.error = undefined;
-        this.$http.post(`${this.config.apiUrl}/documents/remote`, undefined, {
-          params: {type, id},
-        }).then(
-          response => {
+        this.documentItemsApi.upsertFromRemote(type, id)
+          .then(({documentItems}) => {
             this.submitting = false;
-            this.$location.path(`/documents/${response.data.id}`);
-          },
-          response => {
-            this.submitting = false;
-            this.error = response.data && response.data.message ||
-              'unknown reason';
-          },
-        );
+            this.$location.path(`/documents/items/${documentItems[0].id}`);
+          })
+          .finally(() => this.submitting = false);
       }
     },
     template: require('./document-new.html'),
   });
-};
+}
