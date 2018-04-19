7d0765173acdf9aae98c0a71368edda9b785e21c
André Gaul <andre@gaul.io>
2018-04-19 20:51:17 +0200

    channel-selector: add only me channel

--- app/components/channel-selector.ts
+++ app/components/channel-selector.ts
@@ -1,12 +1,14 @@
 export default function(app) {
   app.component('channelSelector', {
     bindings: {
+      documentItem: '<',
       discussions: '<',
     },
     controller: class ChannelSelectorCtrl {
       discussions: any[];
 
       publicDiscussions: number;
+      onlyMeDiscussions: number;
       channelDiscussions: any;
 
       static $inject = ['$scope', '$uibModal', 'authService', 'channelService'];
@@ -16,11 +18,16 @@ export default function(app) {
 
       updateChannelDiscussions() {
         this.publicDiscussions = 0;
+        this.onlyMeDiscussions = 0;
         this.channelDiscussions = {};
 
         if (!this.discussions) return;
 
         this.discussions.forEach(discussion => {
+          if (discussion.onlyMe) {
+            this.onlyMeDiscussions++;
+            return;
+          }
           if (!discussion.channel) {
             this.publicDiscussions++;
             return;
@@ -34,8 +41,8 @@ export default function(app) {
 
       newChannelModalOpen() {
         this.$uibModal.open({component: 'channelNew'});
-      };
+      }
     },
     template: require('./channel-selector.html'),
   });
-};
+}
