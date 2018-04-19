78bd5d235bb6cf9b391f020bd35ff7f73c90e3d0
André Gaul <andre@gaul.io>
2018-04-19 21:56:06 +0200

    channel-selector: select only me if channel not available

--- app/components/channel-selector.ts
+++ app/components/channel-selector.ts
@@ -5,15 +5,57 @@ export default function(app) {
       discussions: '<',
     },
     controller: class ChannelSelectorCtrl {
+      documentItem: any;
       discussions: any[];
 
       publicDiscussions: number;
       onlyMeDiscussions: number;
       channelDiscussions: any;
 
+      channels: any[];
+
       static $inject = ['$scope', '$uibModal', 'authService', 'channelService'];
       constructor($scope, public $uibModal, public authService, public channelService) {
-        $scope.$watchCollection('$ctrl.discussions', this.updateChannelDiscussions.bind(this));
+        $scope.$watch('$ctrl.documentItem', this.update.bind(this));
+        $scope.$watchCollection('$ctrl.documentItem.channelShares', this.update.bind(this));
+        $scope.$watchCollection('$ctrl.discussions', this.update.bind(this));
+      }
+
+      update() {
+        this.updateChannelDiscussions();
+        this.updateChannels();
+        this.updateSelectedChannel();
+      }
+
+      updateChannels() {
+        this.channels = [];
+        if (!this.documentItem) return;
+        if (this.documentItem.public) {
+          this.channels = [...this.channelService.channels];
+        } else {
+          const {channelShares} = this.documentItem || [];
+          this.channels = this.channelService.channels.filter(channel => {
+            if (this.channelDiscussions[channel.id]) return true;
+            if (channelShares.find(share => share.channel === channel.id)) return true;
+            return false;
+          });
+        }
+      }
+
+      updateSelectedChannel() {
+        if (!this.documentItem) return;
+
+        // all channels valid on public document items
+        if (this.documentItem.public) return;
+
+        // only me valid on all document items
+        if (this.channelService.onlyMe) return;
+
+        // current channel in available channels
+        const {selectedChannel} = this.channelService;
+        if (selectedChannel && this.channels.find(channel => channel.id === selectedChannel.id)) return;
+
+        this.channelService.selectOnlyMe();
       }
 
       updateChannelDiscussions() {
