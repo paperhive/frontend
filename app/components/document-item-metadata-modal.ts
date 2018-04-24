c4a3d852216776a0eb85cfa004c8a7b79fe566d9
André Gaul <andre@gaul.io>
2018-04-24 15:11:35 +0200

    document-item-metadata-modal: allow update of authors

--- app/components/document-item-metadata-modal.ts
+++ app/components/document-item-metadata-modal.ts
@@ -1,3 +1,5 @@
+require('./document-item-metadata-modal.less');
+
 import { cloneDeep } from 'lodash';
 
 export default function(app) {
@@ -18,15 +20,31 @@ export default function(app) {
       public succeeded = false;
       public metadata: any;
 
+      author: string;
+
       static $inject = ['$scope', 'documentItemsApi'];
       constructor(public $scope, public documentItemsApi) {
         this.metadata = cloneDeep(this.resolve.documentItem.metadata);
+        if (!this.metadata.authors) this.metadata.authors = [];
+      }
+
+      public addAuthor() {
+        if (!this.author) return;
+        this.metadata.authors.push({name: this.author});
+        delete this.author;
       }
 
       public hasError(field) {
         const form = this.$scope.metadataForm;
         return form && (form.$submitted || form[field].$touched) &&
           form[field].$invalid;
+      }
+
+      public isSubmitDisabled() {
+        return this.submitting
+          || this.succeeded
+          || this.$scope.metadataForm.$invalid
+          || this.metadata.authors.length === 0;
       }
 
       public submit() {
