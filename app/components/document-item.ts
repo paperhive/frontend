2997932df8dcc9af7bbed699239f285c3429ec7b
André Gaul <andre@gaul.io>
2018-05-18 16:45:49 +0200

    document-item: add availableChannels

--- app/components/document-item.ts
+++ app/components/document-item.ts
@@ -202,6 +202,7 @@ export default function(app) {
       documentItem: any;
       documentItems: any[];
       documentSubscriptions: any[];
+      availableChannels: any;
       discussionsCtrl: DiscussionsController;
       discussionsByRevision: any;
       filteredDiscussions: any[];
@@ -222,6 +223,7 @@ export default function(app) {
         $scope.$on('$routeChangeSuccess', this.updateDocumentItem.bind(this));
 
         $scope.$watch('$ctrl.documentItem', this.updateDocumentSubscriptions.bind(this));
+        $scope.$watchCollection('$ctrl.documentItem.channelShares', this.updateAvailableChannels.bind(this));
 
         // update filtered discussions if discussions, channel or showAllChannels changed
         $scope.$watchCollection('$ctrl.discussionsCtrl.discussions', this.updateFilteredDiscussions.bind(this));
@@ -290,7 +292,40 @@ export default function(app) {
           })
           .then(({documentItems}) => {
             this.documentItems = documentItems;
+            this.updateAvailableChannels();
           });
+      }
+
+      updateAvailableChannels() {
+        this.availableChannels = undefined;
+        if (!this.documentItem || !this.documentItems) return;
+        const revisionItems = this.documentItems.filter(item => item.revision === this.documentItem.revision);
+        const hasPublicRevisionItem = !!revisionItems.find(item => item.public);
+
+        if (hasPublicRevisionItem) {
+          this.availableChannels = {
+            public: true,
+            channels: this.channelService.channels && [...this.channelService.channels.map(channel => channel.id)],
+          };
+        } else {
+          // get all channels that are valid for this revision
+          const channels = [];
+          [this.documentItem, this.documentItems.filter(item => item.id !== this.documentItem.id)]
+            .forEach(item => item.channelShares && item.channelShares.forEach(({channel}) => {
+              if (channels.indexOf(channel) === -1) {
+                channels.push(channel);
+              }
+            }));
+
+          this.availableChannels = {public: false, channels};
+        }
+
+        // update selected channel if necessary
+        if (this.channelService.onlyMe) return;
+        if (this.channelService.public && this.availableChannels.public) return;
+        const {selectedChannel} = this.channelService;
+        if (selectedChannel && this.availableChannels.channels.indexOf(selectedChannel.id) !== -1) return;
+        this.channelService.selectOnlyMe();
       }
 
       updateDocumentSubscriptions() {
