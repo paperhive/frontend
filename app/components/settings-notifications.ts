fe1eee2b83ff8fde8d78135d8d186901da02c36d
André Gaul <andre@gaul.io>
2018-08-05 17:19:24 +0200

    settings: move update object function to utils

--- app/components/settings-notifications.ts
+++ app/components/settings-notifications.ts
@@ -1,15 +1,6 @@
 import { cloneDeep } from 'lodash';
 
-export function createPersonUpdate(person) {
-  const { displayName, discipline, occupation, account } = cloneDeep(person);
-  const { createdAt, featureFlags, ...accountUpdate } = account;
-  return {
-    displayName,
-    discipline,
-    occupation,
-    account: {...accountUpdate},
-  };
-}
+import { createPersonUpdate } from '../utils/people';
 
 export default function(app) {
   app.component('settingsNotifications', {
